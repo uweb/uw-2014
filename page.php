6f953253d29b132c1e152d28db898c158d570746
Kilian <kilianf@uw.edu>
2014-07-09 09:49:13 -0700

    Theme LESS updates

--- page.php
+++ page.php
@@ -22,12 +22,113 @@
         <!-- Add your site or application content here -->
 
 
+   
+<!-- Thinstrip -->
 
-<header>
-  <div class="container uw-thinstrip">
+    <header class="uw-thinstrip">
+      <div class="container">
+        <a href="http://www.uw.edu" class="uw-patch">Home</a>
+        <a href="http://www.uw.edu" class="uw-wordmark">Home</a>
+      </div>
+    </header>
 
+
+<!-- Temp filler menu -->
+
+  <div id="dawgdrops" aria-label="Main menu" role="navigation">
+    <h3 class="assistive-text">Main menu</h3>
+
+    <div class="dawgdrops-inner">
+      <ul id="menu-dropdowns" class="dawgdrops-nav" role="menubar">
+        <li role="presentation" class="dawgdrops-item">
+          <a href="#"  class=
+          "dropdown-toggle" title="Discover" aria-controls="menu-discover">Discover</a>
+
+          <ul role="menu" id="menu-discover" aria-expanded="false" class=
+          "dawgdrops-menu"><li><a href="#" tabindex="-1" title="About the UW">About the UW</a></li>
+        <li><a href="#" tabindex="-1"title="Academics &amp; Research">Academics &amp; Research</a></li>
+        <li><a href="#" tabindex="-1" title="Accessible Technology ">Accessible Technology</a></li>
+        <li><a href="#" tabindex="-1" title="Admissions">Admissions</a></li>
+        <li><a href="#" tabindex="-1" title="Arts UW">Arts UW</a></li>
+        <li><a href="#" tabindex="-1" title="Business &amp; Industry">Business &amp;Industry</a></li>
+          </ul>
+        </li>
+
+        <li role="presentation" class="dawgdrops-item">
+          <a href="#" class="dropdown-toggle" title=
+          "Current Students" aria-controls="menu-current-students">Current Students</a>
+
+          <ul role="menu" id="menu-current-students" aria-expanded="false" class=
+          "dawgdrops-menu"><li><a href="#" tabindex="-1"title="Academic Calendar">Academic Calendar</a></li>
+        <li><a href="#" tabindex="-1" title="Advising">Advising</a></li>
+        <li><a href="#" tabindex="-1" title="Career Center">Career Center</a></li>
+        <li><a href="#"tabindex="-1" title="Choosing a major">Choosing a major</a></li>
+        <li><a href="#" tabindex="-1" title="Computing / IT Connect">Computing / IT Connect</a></li>
+        <li><a href="#" tabindex="-1" title="Dining">Dining</a></li>
+          </ul>
+        </li>
+
+        <li role="presentation" class="dawgdrops-item">
+          <a href="#" class=
+          "dropdown-toggle" title="Future Students" aria-controls=
+          "menu-future-students">Future Students</a>
+
+          <ul role="menu" id="menu-future-students" aria-expanded="false" class=
+          "dawgdrops-menu"><li><a href="#" tabindex="-1"title="Admissions">Admissions</a></li>
+        <li><a href="#" tabindex="-1"title="Campus Tours">Campus Tours</a></li>
+        <li><a href="#" tabindex="-1" title="Continuing Education">Continuing Education</a></li>
+        <li><a href="#" tabindex="-1" title="Course Descriptions">Course Descriptions</a></li>
+        <li><a href="#" tabindex="-1" title="Dining">Dining</a></li>
+        <li><a href="#" tabindex="-1" title="Diversity Resources ">Diversity Resources</a></li>
+          </ul>
+        </li>
+
+        <li role="presentation" class="dawgdrops-item">
+          <a href="#" class="dropdown-toggle" title=
+          "Faculty &amp; Staff" aria-controls="menu-faculty-staff">Faculty &amp;
+          Staff</a>
+
+          <ul role="menu" id="menu-faculty-staff" aria-expanded="false" class=
+          "dawgdrops-menu"><li><a href="#" tabindex="-1" title="Alpine / Email">Alpine / Email</a></li>
+        <li><a href="#" tabindex="-1" title="Academic HR">Academic HR</a></li>
+        <li><a href="#" tabindex="-1"title="Administration">Administration</a></li>
+        <li><a href="#" tabindex="-1"title="Benefits &amp; Work / Life">Benefits &amp; Work / Life</a></li>
+        <li><a href="#" tabindex="-1" title="Computing / IT Connect">Computing / IT Connect</a></li>
+        <li><a href="#" tabindex="-1"title="Employee Self Service">Employee Self Service</a></li>
+        <li><a href="#" tabindex="-1" title="Faculty Diversity">Faculty Diversity</a></li>
+        <li><a href="#" tabindex="-1" title="Faculty Senate">Faculty Senate</a></li>
+          </ul>
+        </li>
+
+        <li role="presentation" class="dawgdrops-item">
+          <a href="#" class="dropdown-toggle" title=
+          "Alumni" aria-controls="menu-alumni">Alumni</a>
+
+          <ul role="menu" id="menu-alumni" aria-expanded="false" class="dawgdrops-menu"><li><a href="#" tabindex="-1" title="Alumni Events">Alumni Events</a></li>
+        <li><a href="#" tabindex="-1" title="Alumni Services">Alumni Services</a></li>
+        <li><a href="#" tabindex="-1" title="Alumni Tours">Alumni Tours</a></li>
+        <li><a href="#" tabindex="-1" title="Columns Magazine">Columns Magazine</a></li>
+        <li><a href="#" tabindex="-1" title="Connect with other Alumni">Connect with other Alumni</a></li>
+        <li><a href="#" tabindex="-1" title="Lifelong Learning">Lifelong Learning</a></li>
+          </ul>
+        </li>
+
+        <li role="presentation" class="dawgdrops-item">
+          <a href="#" class="dropdown-toggle" title=
+          "NW Neighbors" aria-controls="menu-nw-neighbors">NW Neighbors</a>
+
+          <ul role="menu" id="menu-nw-neighbors" aria-expanded="false" class=
+          "dawgdrops-menu"><li><a href="#" tabindex="-1" title="Arts UW Ticket Office">Arts UW Ticket Office</a></li>
+        <li><a href="#" tabindex="-1" title="Botanic Gardens">Botanic Gardens</a></li>
+        <li><a href="#" tabindex="-1" title="Burke Museum Visitor Info">Burke Museum VisitorInfo</a></li>
+        <li><a href="#" tabindex="-1" title="Henry Art Gallery Visitor Info">Henry Art GalleryVisitor Info</a></li>
+        <li><a href="#" tabindex="-1" title="Husky Sports Ticket Office">Husky Sports TicketOffice</a></li>
+          </ul>
+        </li>
+
+      </ul>
+    </div>
   </div>
-</header>
 
 
 
