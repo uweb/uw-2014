03b89997cb2f24820d9ce3511ed856bc69f68e6e
Jon Swanson <jswan2@uw.edu>
2014-07-10 10:52:59 -0700

    start of child items in dropdowns

--- setup/dropdowns.php
+++ setup/dropdowns.php
@@ -25,16 +25,25 @@ class UW_Dropdowns
 
   function install_default_white_bar_menu()
   {
-
     $menu_items = $this->menu_list();
     $this->MENU_ID = wp_create_nav_menu( self::DISPLAY_NAME );
 
     // wp_create_nav_menu returns a WP_Error if the menu already exists;
     if ( is_wp_error( $this->MENU_ID ) ) return;
 
-    foreach ( $menu_items as $menu_name => $menu_attributes )
-    {
-      wp_update_nav_menu_item( $this->MENU_ID, $menu_item_db_id=0, $menu_attributes );
+
+    //      Each site in the network will have a different menu item ID for each thing.  Make the first menu and save its id
+    //      then set that ID as the menu-item-parent-id for each child.  Then save each child.
+    //      We can add another layer of depth for grandchildren
+    foreach ( $menu_items as $menu_item ) {
+        $menu_item_id = wp_update_nav_menu_item($this->MENU_ID. $menu_item_db_id=0, $menu_item->args);
+        if (!empty($menu_item->child_items)) {
+            foreach ( $menu_item->child_items as $child_item ){
+                wp_die(print_r($child_item));
+                $child_item->args['menu-item-parent-id'] = $menu;
+                wp_update_nav_menu_item($this->MENU_ID, $menu_item_db_id=0, $child_item->args);
+            }
+        }
     }
 
     $this->set_uw_menu_location();
@@ -52,11 +61,31 @@ class UW_Dropdowns
 
     return array(
 
-      'Discover' => array(
-        'menu-item-title'  => __('Discover'),
-        'menu-item-url'    => 'http://uw.edu/discover/',
-        'menu-item-status' => 'publish',
-      ),
+        'Discover' => array(
+            'args' => array(
+                'menu-item-title'  => __('Discover'),
+                'menu-item-url'    => 'http://uw.edu/discover/',
+                'menu-item-status' => 'publish',
+            ),
+
+            'child_items' => array(
+                'Vision and Values' => array(
+                    'args' => array(
+                        'menu-item-title'       => __('Vision and Values'),
+                        'menu-item-url'         => 'http://uw.edu/discover/visionvalues',
+                        'menu-item-status'      => 'publish',
+                    ),
+                ),
+                'Mission Statement' => array(
+                    'args' => array(
+                        'menu-item-title'       => __('Mission Statement'),
+                        'menu-item-url'         => 'http://uw.edu/admin/rules/policies/BRG/RP5.html',
+                        'menu-item-status'      => 'publish',
+                    ),
+                ),
+            ),
+        ),
+
 
       'Current Students' => array(
         'menu-item-title'  => __('Current Students'),
