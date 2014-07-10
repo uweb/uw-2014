c7f76412792f8cdffad18fbf8e4efc80a6c4f12d
Kilian <kilianf@uw.edu>
2014-07-09 17:35:43 -0700

    SVG additions and CSS changes

--- page.php
+++ page.php
@@ -1,4 +1,21 @@
 <?php get_header(); ?>
 
 
+
+<div class="uw-hero-image">
+    
+</div>
+
+
+
+<div class="container body-content">
+  
+</div>
+
+
+
+
+
+
+
 <?php get_footer(); ?>
--- thinstrip.php
+++ thinstrip.php
@@ -1,4 +1,13 @@
 <header class="uw-thinstrip">
+   
+  <div class="uw-thin-strip-nav">
+      <ul class="uw-audience">
+    	<li><a href="#">Faculty & Staff</a></li>
+    	<li><a href="#">Admissions</a></li>
+    	<li><a href="#">Students</a></li>
+    </ul>
+  </div>
+
   <div class="container">
     <a href="http://www.uw.edu" class="uw-patch">Home</a>
     <a href="http://www.uw.edu" class="uw-wordmark">Home</a>
