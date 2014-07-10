844678ac8d3d279e7b9dc75599ec46c988ff68b3
Kilian <kilianf@uw.edu>
2014-07-10 15:05:08 -0700

    Style additions

--- thinstrip.php
+++ thinstrip.php
@@ -1,7 +1,7 @@
 <header class="uw-thinstrip">
 
   <div class="uw-thin-strip-nav">
-      <ul class="uw-audience">
+      <ul class="uw-thin-links">
       	<li class="uw-quicklinks"><a href="#">Quick Links</a></li>
      	<li class="uw-search"><a href="#">Search</a></li>
       	<li><a href="#">Students</a></li>
