9a41062a702636ac152fea2506ebfe34e0abf1ee
Jon Swanson <jswan2@uw.edu>
2014-07-28 11:09:30 -0700

    added little links (if there are links without classes aka icons)

--- js/uw.quicklinks.js
+++ js/uw.quicklinks.js
@@ -1,6 +1,8 @@
 // This section builds and populates the quicklinks section (off-canvas right)
 
 UW.QuickLink = Backbone.Model.extend({
+    has_icon: false,
+    classes: [],
 });
 
 UW.QuickLinks = Backbone.Collection.extend({
@@ -15,26 +17,22 @@ UW.QuickLinks = Backbone.Collection.extend({
     },
 
     default_links: [
-        {text: 'Maps', link_url: 'http://uw.edu/maps', classes: ['icon-maps']},
-        {text: 'Directories', link_url: 'http://uw.edu/directory', classes: ['icon-directories']},
-        {text: 'Calendar', link_url: 'http://uw.edu/calendar', classes: ['icon-calendar']},
-        {text: 'Libraries', link_url: 'http://uw.edu/libraries', classes: ['icon-libraries']},
-        {text: 'MyUW', link_url: 'http://myuw.washington.edu', classes: ['icon-myuw']},
-        {text: 'UW Today', link_url: 'http://uw.edu/news', classes: ['icon-uwtoday']},
+        {title: 'Maps', url: 'http://uw.edu/maps', classes: ['icon-maps']},
+        {title: 'Directories', url: 'http://uw.edu/directory', classes: ['icon-directories']},
+        {title: 'Calendar', url: 'http://uw.edu/calendar', classes: ['icon-calendar']},
+        {title: 'Libraries', url: 'http://uw.edu/libraries', classes: ['icon-libraries']},
+        {title: 'MyUW', url: 'http://myuw.washington.edu', classes: ['icon-myuw']},
+        {title: 'UW Today', url: 'http://uw.edu/news', classes: ['icon-uwtoday']},
        ],
 
     use_ajax: function (holder, response) {
+        this.models = [];
         if (Object.keys(response).length === 0){
             this.use_defaults();
         }
         else {
-            this.models = [];
             for (var key in response) {
-                if (response[key].classes.length > 0){
-                    this.models.push(new this.model({text: response[key].title, link_url: response[key].url, classes: response[key].classes}));
-                }
-                else {
-                    
+                this.add_model(response[key]); 
             }
             this.make_view();
         }
@@ -43,9 +41,18 @@ UW.QuickLinks = Backbone.Collection.extend({
     use_defaults: function () {
         this.models = [];
         for (var i = 0; i < this.default_links.length; i++){
-            this.models.push(new this.model({text: this.default_links[i].text, link_url: this.default_links[i].link_url, classes: this.default_links[i].classes}));
+            this.add_model(this.default_links[i]);
         }
         this.make_view();
+    },
+
+    add_model: function (link) {
+        if (link.classes !== 'undefined' && link.classes.length > 0 && link.classes[0] !== '') {
+            this.models.push(new this.model({text: link.title, link_url: link.url, classes: link.classes, has_icon: true }));
+        }
+        else {
+            this.models.push(new this.model({text: link.title, link_url: link.url,}));
+        }
     },
 
     make_view: function () {
@@ -58,7 +65,7 @@ UW.QuickLinksView = Backbone.View.extend({
 
     container: 'div#uw-container',
 
-    menu_item : '<li><span class="<%= classes %>"></span><a href="<%= url %>"><%= text %></a></li>',
+    menu_item : '<li><% if (classes) { %><span class="<%= classes %>"></span><% } %><a href="<%= url %>"><%= text %></a></li>',
 
     events: {
        'click': 'animate'
@@ -77,23 +84,34 @@ UW.QuickLinksView = Backbone.View.extend({
             UW.$body.children().not('#wpadminbar').not('script').wrapAll('<div id="uw-container"><div id="uw-container-inner"></div></div>');
             this.$container = $(this.container);
         }
-        this.$container.prepend("<nav id='quicklinks'><ul></ul></nav>");
+        this.$container.prepend("<nav id='quicklinks'><ul id='big_links'></ul></nav>");
         this.$drawer = $('nav#quicklinks');
+        this.$list = this.$drawer.find('ul#big_links');
+        this.$little_list = false;
     },
 
     add_links: function () {
-        this.$list = this.$drawer.find('ul');
         _.each( this.collection.models, this.append_menu_item )
     },
 
     append_menu_item : function( model )
     {
         item = {
-            classes: model.get('classes').join(' '),
+            classes: false,
             url: model.get('link_url'),
             text: model.get('text')
         };
-        this.$list.append( _.template( this.menu_item, item ) )
+        if (model.get('has_icon')) {
+            item.classes = model.get('classes').join(' ');
+            this.$list.append( _.template( this.menu_item, item ) );
+        }
+        else {
+            if (!this.$little_list) {
+                this.$drawer.append('<h3>Helpful Links</h3><ul id="little_links"></ul>');
+                this.$little_list = this.$drawer.find('ul#little_links');
+            }
+            this.$little_list.append(_.template(this.menu_item, item));
+        }
     },
 
     animate: function () {
