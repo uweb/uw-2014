f1a48ab68446f9494d514a0091bad2227f96b262
Jon Swanson <jswan2@uw.edu>
2014-08-08 15:55:50 -0700

    added big hero and no hero page templates

---
+++ page-no-hero.php
@@ -0,0 +1,49 @@
+<?php 
+/*
+Template Name: No Hero
+ */
+
+get_header();
+?>
+
+		<div role="main" class="container uw-body">
+			
+						
+			<div class="row">
+				<div class="col-md-8 uw-content">
+                    <h2 class='uw-site-title'><?php bloginfo(); ?></h2>
+					
+                    <?php
+                    while ( have_posts() ) : the_post();
+                    
+                    uw_breadcrumbs();
+                    ?>
+
+			        <main id="post-<?php the_id(); ?>" <?php post_class(); ?>>
+				        <header class="entry-header">
+                            <?php
+                            if (!empty(get_the_title())):
+                            ?>
+					        <h1 class="entry-title"><?php the_title(); ?></h1>
+					        <?php endif; ?>
+				        </header><!-- .entry-header -->
+				        <div class="entry-content">
+                            <?php the_content(); ?>
+                        </div><!-- .entry-content -->
+                        <footer class="entry-meta">
+                            <?php edit_post_link( __( 'Edit', 'uw' ), '<span class="edit-link">', '</span>' ); ?>
+                        </footer><!-- .entry-meta -->
+                    </main><!-- #post-<?php the_id(); ?> -->
+
+					<?php comments_template( '', true ); ?>
+
+                    <?php endwhile; // end of the loop. ?>
+
+                </div><!-- uw-content -->
+				<div class="col-md-4 uw-sidebar" role="complementary">
+                    <?php uw_sidebar(); ?>
+                </div><!-- uw-sideabr -->
+			</div><!-- row -->
+ 		</div><!-- uw-body -->
+
+<?php get_footer(); ?>
