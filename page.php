0ea160157c4401f931d5a1b56b63413223aedf4e
Jon Swanson <jswan2@uw.edu>
2014-08-08 12:44:38 -0700

    refactored page.php

--- page.php
+++ page.php
@@ -10,26 +10,29 @@
 				<div class="col-md-8 uw-content">
                     <h2 class='uw-site-title'><?= get_bloginfo()?></h2>
 					
-			        <?php while ( have_posts() ) : the_post(); ?>
+                    <?php while ( have_posts() ) :
+                    the_post();
+                    $post_id = get_the_id();
+                    $title = get_the_title();
+                    
+                    uw_breadcrumbs();
+                    ?>
 
-                    <?php uw_breadcrumbs(); ?>
-
-			        <main id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			        <main id="post-<?= $post_id ?>" <?php post_class(); ?>>
 				        <header class="entry-header">
                             <?php
-                            $title = get_the_title();
-					        if (!empty($title)): ?>
-					        <h1 class="entry-title"><?php echo apply_filters('italics', $title); ?></h1>
+                            if (!empty($title)):
+                            ?>
+					        <h1 class="entry-title"><?= $title ?></h1>
 					        <?php endif; ?>
 				        </header><!-- .entry-header -->
 				        <div class="entry-content">
-                            <?php the_content(); ?>
-                            <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'uw' ) . '</span>', 'after' => '</div>' ) ); ?>
+                            <?= $content ?>
                         </div><!-- .entry-content -->
                         <footer class="entry-meta">
                             <?php edit_post_link( __( 'Edit', 'uw' ), '<span class="edit-link">', '</span>' ); ?>
                         </footer><!-- .entry-meta -->
-                    </main><!-- #post-<?php the_ID(); ?> -->
+                    </main><!-- #post-<?= $post_id ?> -->
 
 					<?php comments_template( '', true ); ?>
 
