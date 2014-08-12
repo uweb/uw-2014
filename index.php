bbbf2fa7a44a84c8dbdc603b23c1bdb04c3fd6ff
Dane Odekirk <dizzzy@uw.edu>
2014-08-12 13:46:51 -0700

    Moving template files away from lorem ipsum

--- index.php
+++ index.php
@@ -1,74 +1,48 @@
 <?php get_header(); ?>
 
-<div class="uw-hero-image"> </div>
-
+<div class="uw-hero-image"></div>
 
 <div class="container uw-body">
 
+  <div class="row">
 
-<div class="row">
+    <div class="col-md-8 uw-content">
 
-  <div class="col-md-8 uw-content">
-      <h2 class="uw-site-title">The Office <span>of the</span> President</h2>
+      <h2 class="uw-site-title"><?php bloginfo(); ?></h2>
 
       <?php uw_breadcrumbs() ?>
+      <?php uw_mobile_menu() ?>
 
       <div class="uw-body-copy">
 
-        <h1>Enim varius iaculis a massa</h1>
+  			<?php
+  				// Start the Loop.
+  				while ( have_posts() ) : the_post();
 
-        <?php uw_mobile_menu() ?>
+  					/*
+  					 * Include the post format-specific template for the content. If you want to
+  					 * use this in a child theme, then include a file called called content-___.php
+  					 * (where ___ is the post format) and that will be used instead.
+  					 */
+  					get_template_part( 'content', get_post_format() );
 
-        <p>Consectetur adipiscing elit. Sed a massa eget enim varius iaculis eget auctor justo. Aenean eu erat leo. Praesent <a href="#">vitae suscipit</a> mauris. Nullam faucibus pellentesque nisl vitae elementum. Cras gravida tempor sem quis iaculis. Nullam in ante condimentum magna mollis aliquet non vel purus. Cras eget erat nisl. Integer vitae nunc in magna aliquam ornare. Mauris vitae dapibus turpis, at malesuada felis. Sed tristique tellus id lacinia bibendum. Sed sed tortor vel felis vestibulum venenatis. Mauris tristique urna nec purus pretium, vel euismod est pharetra. Aliquam neque eros, vulputate eget pellentesque a, convallis quis enim. Praesent sollicitudin tempor fringilla. Fusce imperdiet felis at rutrum rhoncus. Nulla pharetra porttitor ante ut pretium.</p>
-        <img class="alignleft" src="http://127.0.0.1/cms/wp-content/themes/uw-2014/assets/temp/pres.jpg"><p>Consectetur adipiscing elit. Sed a massa eget enim varius iaculis eget auctor justo. Aenean eu erat leo. Praesent <a href="#">vitae suscipit</a> mauris. Nullam faucibus pellentesque nisl vitae elementum. Cras gravida tempor sem quis iaculis. Nullam in ante condimentum magna mollis aliquet non vel purus. Cras eget erat nisl. Integer vitae nunc in magna aliquam ornare. Mauris vitae dapibus turpis, at malesuada felis. Sed tristique tellus id lacinia bibendum. Sed sed tortor vel felis vestibulum venenatis. Mauris tristique urna nec purus pretium, vel euismod est pharetra. Aliquam neque eros, vulputate eget pellentesque a, convallis quis enim. Praesent sollicitudin tempor fringilla. Fusce imperdiet felis at rutrum rhoncus. Nulla pharetra porttitor ante ut pretium.</p>
-        <p>Consectetur adipiscing elit. Sed a massa eget enim varius iaculis eget auctor justo. Aenean eu erat leo. Praesent <a href="#">vitae suscipit</a> mauris. Nullam faucibus pellentesque nisl vitae elementum. Cras gravida tempor sem quis iaculis. Nullam in ante condimentum magna mollis aliquet non vel purus. Cras eget erat nisl. Integer vitae nunc in magna aliquam ornare. Mauris vitae dapibus turpis, at malesuada felis. Sed tristique tellus id lacinia bibendum. Sed sed tortor vel felis vestibulum venenatis. Mauris tristique urna nec purus pretium, vel euismod est pharetra. Aliquam neque eros, vulputate eget pellentesque a, convallis quis enim. Praesent sollicitudin tempor fringilla. Fusce imperdiet felis at rutrum rhoncus. Nulla pharetra porttitor ante ut pretium.</p>
+  					// If comments are open or we have at least one comment, load up the comment template.
+  					if ( comments_open() || get_comments_number() ) {
+  						comments_template();
+  					}
 
-             <!-- <label class="radio">
-              <input type="radio" name="group1" value="1" data-toggle="radio">
-              Radio is off
-            </label>
-            <label class="radio">
-              <input type="radio" name="group1" value="2" data-toggle="radio" checked>
-              Radio is on
-            </label>
-            <label class="radio">
-              <input type="radio" name="group2" value="1" data-toggle="radio" disabled>
-              Radio is off
-            </label>
-            <label class="radio">
-              <input type="radio" name="group2" value="2" data-toggle="radio" disabled checked>
-              Radio is on
-            </label>
- -->
-            <div id="singlevid" class="uw-youtube" data-uw-youtube="RtWbpyjJqrU" data-uw-youtube-type="single"></div>
-            <div id="kapow" class="uw-youtube" data-uw-youtube="PL6EC7B047181AD013" data-uw-youtube-type="playlist" data-max-results='50'></div>
-            <div class="uw-player">
-              <video class="video-js"
-                controls preload="auto" width="100%" height="300px"
-                poster="http://video-js.zencoder.com/oceans-clip.png"
-                data-setup='{"example_option":true}'>
-               <source src="http://video-js.zencoder.com/oceans-clip.mp4" type='video/mp4' />
-               <source src="http://video-js.zencoder.com/oceans-clip.webm" type='video/webm' />
-               <source src="http://video-js.zencoder.com/oceans-clip.ogv" type='video/ogg' />
-               <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
-              </video>
-            </div>
+  				endwhile;
+  			?>
+
+
       </div>
 
-  </div>
+    </div>
 
-  <div class="col-md-4 uw-sidebar">
-    <?php uw_sidebar(); ?>
-    <?php dynamic_sidebar( UW_Sidebar::ID ); ?>
+    <?php get_sidebar() ?>
+
   </div>
 
 </div>
-
-
-
-
-</div>
-
-
 
 <?php get_footer(); ?>
