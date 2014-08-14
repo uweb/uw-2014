2b196023d50fdaf0be3e14b0ab0aaec0bb936eab
Kilian <kilianf@uw.edu>
2014-08-14 13:35:06 -0700

    Merge files

--- footer.php
+++ footer.php
@@ -1,8 +1,36 @@
 <?php wp_footer(); ?>
 
-<div class="uw-footer">
+	<div class="uw-footer">
+	
+		<a href="" class="footer-wordmark">University of Washington</a>
 
-</div>
+		<h4>Connect with us:</h4>
+		<ul class="footer-social">
+			<li><a class="facebook" href="">Facebook</a></li>
+			<li><a class="twitter" href="">Twitter</a></li>
+			<li><a class="instagram" href="">Pinterest</a></li>
+			<li><a class="youtube" href="">YouTube</a></li>
+			<li><a class="linkedin" href="">LinkedIn</a></li>
+			<li><a class="pinterest" href="">Vine</a></li>
+			<li><a class="vine" href="">Google+</a></li>
+			<li><a class="google" href="">Instagram</a></li>
+		</ul>
+
+		<ul class="footer-links">
+			<li><a href="">Accessibility</a></li>
+			<li><a href="">Contact Us</a></li>
+			<li><a href="">Jobs</a></li>
+			<li><a href="">Campus Safety</a></li>
+			<li><a href="">My UW</a></li>
+			<li><a href="">Rules Docket</a></li>
+			<li><a href="">Privacy Terms</a></li>
+		</ul>
+
+         
+        <p>&copy;	 2014 University of Washington  |  Seattle, WA</p>
+
+	
+	</div>
 
 </body>
 </html>
