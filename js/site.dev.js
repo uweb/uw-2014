d176fd0ffe37da915671d161295f9e3f5965e679
Kilian <kilianf@uw.edu>
2014-08-15 16:56:13 -0700

    Merge

--- js/site.dev.js
+++ js/site.dev.js
@@ -15856,7 +15856,7 @@ UW.Dropdowns = Backbone.View.extend({
   {
     var $el = $(el.currentTarget)
       , position = $el.position()
-    $el.find('ul').css( { top : position.top + 74, left: position.left } )
+    $el.find('ul').css( { top : position.top + 58, left: position.left } )
   },
 
   toggleSubMenu : function( e )
