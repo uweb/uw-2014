c067958bf61b832eb44683ad48b0588031899818
Jon Swanson <jswan2@uw.edu>
2014-08-18 14:48:43 -0700

    added optional max_results attribute to youtube shortcode

--- setup/class.youtube-shortcode.php
+++ setup/class.youtube-shortcode.php
@@ -2,7 +2,7 @@
 
 /*
  *  YouTube shortcode allows for youtube video and/or playlist to be added to content
- *  [youtube type='type' id='video or playlist id']
+ *  [youtube type='type' id='video or playlist id' max-results=<integer that overrides max results>]
  */
 
 class UW_YouTube
@@ -39,7 +39,14 @@ class UW_YouTube
         }
 
         $el_id = 'uw-youtube-' . rand(0,100); //needs unique id.  Could set this in shortcode instead of generating
+        $max_results = intval($attributes->max_results);
+        if ($max_results > 0){
+            $return = sprintf('<div id="%s" class="uw-youtube" data-uw-youtube-type="%s" data-uw-youtube="%s" data-max-results="%d"></div>', $el_id, $type, $id, $max_results);        
+        }
+        else {
+            $return = sprintf('<div id="%s" class="uw-youtube" data-uw-youtube-type="%s" data-uw-youtube="%s"></div>', $el_id, $type, $id);
+        }
 
-        return sprintf('<div id="%s" class="uw-youtube" data-uw-youtube-type="%s" data-uw-youtube="%s"></div>', $el_id, $type, $id);
+        return $return;
     }
 }
