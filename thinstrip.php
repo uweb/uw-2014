59cbfcdb7451cc8e5d65e9df7a9d563fcc8f6347
Jon Swanson <jswan2@uw.edu>
2014-08-26 10:26:48 -0700

    expanded the hoverable area to the whole top/right bounded by graphic

--- thinstrip.php
+++ thinstrip.php
@@ -2,7 +2,7 @@
 
   <div class="uw-thin-strip-nav">
       <ul class="uw-thin-links">
-      	<li class="uw-quicklinks"><a href="#">Quick Links</a></li>
+      	<li class="uw-quicklinks"><div></div><a href="#">Quick Links</a></li>
      	<li class="uw-search"><a href="#">Search</a></li>
       <li><a href="#">Alumni</a></li>
       <li><a href="#">Faculty & Staff</a></li>
