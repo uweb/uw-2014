bf6fe4453cd82b2f41f1e948bb114461bf643ce7
Jon Swanson <jswan2@uw.edu>
2014-08-27 15:09:22 -0700

    fixed an indentation annoyance

--- thinstrip.php
+++ thinstrip.php
@@ -4,9 +4,9 @@
       <ul class="uw-thin-links">
       	<li class="uw-quicklinks"><div></div><a href="#">Quick Links</a></li>
      	<li class="uw-search"><a href="#">Search</a></li>
-      <li><a href="#">Alumni</a></li>
-      <li><a href="#">Faculty & Staff</a></li>
-      <li><a href="#">Parents</a></li>
+        <li><a href="#">Alumni</a></li>
+        <li><a href="#">Faculty & Staff</a></li>
+        <li><a href="#">Parents</a></li>
     	<li><a href="#">Students</a></li>
       </ul>
   </nav>
