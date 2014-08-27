4988cf1b019dbb84cf4afbebdbbb611caaa0a28c
Jon Swanson <jswan2@uw.edu>
2014-08-27 11:08:32 -0700

    made footer links as accessible as possible

--- footer.php
+++ footer.php
@@ -5,28 +5,31 @@
 		<a href="" class="footer-wordmark">University of Washington</a>
 
 		<h4>Connect with us:</h4>
-		<ul class="footer-social">
-			<li><a class="facebook" href="">Facebook</a></li>
-			<li><a class="twitter" href="">Twitter</a></li>
-			<li><a class="instagram" href="">Pinterest</a></li>
-			<li><a class="youtube" href="">YouTube</a></li>
-			<li><a class="linkedin" href="">LinkedIn</a></li>
-			<li><a class="pinterest" href="">Vine</a></li>
-			<li><a class="vine" href="">Google+</a></li>
-			<li><a class="google" href="">Instagram</a></li>
-		</ul>
+        <nav role="navigation" aria-label="social networking">
+            <ul class="footer-social">
+                <li><a class="facebook" href="">Facebook</a></li>
+                <li><a class="twitter" href="">Twitter</a></li>
+                <li><a class="instagram" href="">Pinterest</a></li>
+                <li><a class="youtube" href="">YouTube</a></li>
+                <li><a class="linkedin" href="">LinkedIn</a></li>
+                <li><a class="pinterest" href="">Vine</a></li>
+                <li><a class="vine" href="">Google+</a></li>
+                <li><a class="google" href="">Instagram</a></li>
+            </ul>
+        </nav>
 
-		<ul class="footer-links">
-			<li><a href="">Accessibility</a></li>
-			<li><a href="http://www.washington.edu/home/siteinfo/form">Contact Us</a></li>
-			<li><a href="http://www.washington.edu/jobs">Jobs</a></li>
-			<li><a href="http://www.uw.edu/safety">Campus Safety</a></li>
-			<li><a href="https://myuw.washington.edu/">My UW</a></li>
-			<li><a href="http://www.washington.edu/admin/rules/wac/rulesindex.html">Rules Docket</a></li>
-			<li><a href="http://www.washington.edu/online/privacy">Privacy</a></li>
-			<li><a href="http://www.washington.edu/online/terms">Terms</a></li>
-		</ul>
-
+        <nav role="navigation" aria-label="footer links">
+            <ul class="footer-links">
+                <li><a href="">Accessibility</a></li>
+                <li><a href="http://www.washington.edu/home/siteinfo/form">Contact Us</a></li>
+                <li><a href="http://www.washington.edu/jobs">Jobs</a></li>
+                <li><a href="http://www.uw.edu/safety">Campus Safety</a></li>
+                <li><a href="https://myuw.washington.edu/">My UW</a></li>
+                <li><a href="http://www.washington.edu/admin/rules/wac/rulesindex.html">Rules Docket</a></li>
+                <li><a href="http://www.washington.edu/online/privacy">Privacy</a></li>
+                <li><a href="http://www.washington.edu/online/terms">Terms</a></li>
+            </ul>
+        </nav>
          
         <p>&copy;	 2014 University of Washington  |  Seattle, WA</p>
 
