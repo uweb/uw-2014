68f527a7bda838fbb02720dc9925b410badcab7e
Jon Swanson <jswan2@uw.edu>
2014-08-28 11:49:43 -0700

    changed button-like things in thinstrip to button

--- thinstrip.php
+++ thinstrip.php
@@ -2,8 +2,8 @@
 
   <nav class="uw-thin-strip-nav" role='navigation' aria-label='Navigate by audience'>
       <ul class="uw-thin-links">
-      	<li class="uw-quicklinks"><div></div><a href="#">Quick Links</a></li>
-     	<li class="uw-search"><a href="#">Search</a></li>
+      	<li class="uw-quicklinks"><div></div><button aria-has-popup='true' aria-owns='quicklinks' aria-controls='quicklinks'>Quick Links</button></li>
+     	<li class="uw-search"><button aria-ha-popup='true' aria-owns='searcharea' aria-opens='searcharea'>Search</button></li>
         <li><a href="#">Alumni</a></li>
         <li><a href="#">Faculty & Staff</a></li>
         <li><a href="#">Parents</a></li>
