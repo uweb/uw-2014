a9a81c2b89a7e979c0139b4c033d92f0ae9a87f9
Dane Odekirk <dizzzy@uw.edu>
2014-09-02 11:23:28 -0700

    Adding image mime type template.

---
+++ content-image.php
@@ -0,0 +1,14 @@
+
+<div class="uw-image-content">
+
+  <?php echo wp_get_attachment_image($post->ID, 'full', false, $attr=array('class' =>'attachment-full center-block')); ?>
+
+  <h1 class="entry-title"><?php the_title(); ?></h1>
+
+  <?php the_excerpt(); ?>
+
+  <div>
+    <a href="<?php echo wp_get_attachment_url(get_the_ID());?>" title="<?php the_title(); ?>" target="_blank" download="<?php the_title() ?>">Download</a>
+  </div>
+
+</div>
