0c78f0c3b5e9004590221782e01d0a32ddf00ef8
Dane Odekirk <dizzzy@uw.edu>
2014-09-03 14:38:07 -0700

    Adding searchform

---
+++ searchform.php
@@ -0,0 +1,7 @@
+<form role="search" method="get" id="searchform" class="searchform" action="http://128.208.132.94/cms/news/">
+	<div>
+		<label class="screen-reader-text" for="s">Search for:</label>
+		<input type="text" value="" name="s" id="s" placeholder="Search for:">
+		<input type="submit" id="searchsubmit" value="Search">
+	</div>
+</form>
