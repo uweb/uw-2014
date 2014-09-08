675817c1a41253ed18155b74c2f7e8579067b3a2
Dane Odekirk <dizzzy@uw.edu>
2014-09-08 13:08:21 -0700

    Added site name to body class.

--- setup/class.filters.php
+++ setup/class.filters.php
@@ -1,16 +1,32 @@
 <?php
+// UW Filters
+//
+// These are filters the UW 2014 theme adds and globally uses.
 
 class UW_Filters
 {
 
-  function UW_Filters()
+  function __construct()
   {
 
+    // Custom UW Filters
     add_filter( 'italics', array( $this, 'italicize') );
     add_filter( 'abbreviation', array( $this, 'abbreviate') );
 
+    // Global filters
     // allow shortcodes in text widgets
     add_filter( 'widget_text', 'do_shortcode' );
+    add_filter( 'body_class', array( $this, 'uw_custom_body_classes' ) );
+
+  }
+
+  function uw_custom_body_classes( $classes )
+  {
+
+    if ( is_multisite() )
+        $classes[] = 'site-'. sanitize_html_class( str_replace( 'cms', '', get_blog_details( get_current_blog_id() )->path ) );
+
+    return $classes;
 
   }
 
