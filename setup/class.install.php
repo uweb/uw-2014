b87ba6e54a252465b045fe90768dd20d1268c1a5
Dane Odekirk <dizzzy@uw.edu>
2014-09-09 15:47:18 -0700

    Adding custom header functionality.

--- setup/class.install.php
+++ setup/class.install.php
@@ -3,9 +3,8 @@
  * Sets up the theme
  *  - basic settings
  *  - header images
- *  - navigation locations
- *  - widget locations
  *  - post thumbnails
+ *  - automatic feed links
  */
 
 class UW_Install_Theme
@@ -13,93 +12,38 @@ class UW_Install_Theme
 
   public $DEFAULT_HEADERS = array(
       'blossoms' => array(
-        'url' => '%s/img/header/cherries.jpg',
-        'thumbnail_url' => '%s/img/header/cherries-thumbnail.jpg',
-        'description' => 'Cherry Blossoms'
-      ),
-      'cherries' => array(
-        'url' => '%s/img/header/cherries-II.jpg',
-        'thumbnail_url' => '%s/img/header/cherries-II-thumbnail.jpg',
-        'description' => 'Cherry Blossoms II'
-      ),
-      'fern' => array(
-        'url' => '%s/img/header/fern.jpg',
-        'thumbnail_url' => '%s/img/header/fern-thumbnail.jpg',
-        'description' => 'Fern'
-      ),
-      'globe' => array(
-        'url' => '%s/img/header/globe.jpg',
-        'thumbnail_url' => '%s/img/header/globe-thumbnail.jpg',
-        'description' => 'Globe'
-      ),
-      'grass' => array(
-        'url' => '%s/img/header/grass.jpg',
-        'thumbnail_url' => '%s/img/header/grass-thumbnail.jpg',
-        'description' =>  'Grass'
-      ),
-      'grill' => array(
-        'url' => '%s/img/header/grill.jpg',
-        'thumbnail_url' => '%s/img/header/grill-thumbnail.jpg',
-        'description' => 'Grill'
-      ),
-      'lights' => array(
-        'url' => '%s/img/header/lights.jpg',
-        'thumbnail_url' => '%s/img/header/lights-thumbnail.jpg',
-        'description' => 'Lights'
-      ),
-      'reeds' => array(
-        'url' => '%s/img/header/reeds.jpg',
-        'thumbnail_url' => '%s/img/header/reeds-thumbnail.jpg',
-        'description' => 'Reeds'
-      ),
-      'suzzallo' => array(
-        'url' => '%s/img/header/suzzallo.jpg',
-        'thumbnail_url' => '%s/img/header/suzzallo-thumbnail.jpg',
-        'description' => 'Suzzallo'
-      ),
+        'url'           => '%s/assets/headers/ima.jpg',
+        'thumbnail_url' => '%s/assets/headers/ima-thumbnail.jpg',
+        'description'   => 'IMA'
+      )
   );
 
   function __construct()
   {
     add_action( 'after_setup_theme', array( $this, 'uw_setup' ) );
-    add_filter( 'bloginfo_rss', array( $this, 'uw_category_rss_link'), 10, 2);
   }
 
   function uw_setup()
   {
-    $defaultImage = reset($this->DEFAULT_HEADERS);
+
+    $defaultImage = reset( $this->DEFAULT_HEADERS );
+
 	  add_theme_support( 'automatic-feed-links' );
+
 	  add_theme_support( 'post-thumbnails' );
+
     add_theme_support( 'custom-header', array(
       'default-image'  => $defaultImage['url'],
       'random-default' => false,
-      'header-text' => false,
-      'width'       => 1280,
-      'height'      => 193,
-      'header-text' => false
+      'header-text'    => false,
+      'width'          => 1600,
+      'height'         => 343,
+      'header-text'    => false,
+      'uploads'        => false
     ) );
 
     register_default_headers( $this->DEFAULT_HEADERS );
 
-    $this->register_navigation_menus();
-
-  }
-
-  function register_navigation_menus()
-  {
-	  // register_nav_menu( 'primary', __( 'Primary Menu', 'uw' ) );
-	  // register_nav_menu( 'footer', __( 'Footer Menu', 'uw' ) );
-  }
-
-  function uw_category_rss_link($arg, $show)
-  {
-    if ($show == 'url' && is_feed() && is_category() )
-    {
-      $id = get_query_var('cat');
-      return get_category_link($id);
-    }
-
-    return $arg;
   }
 
 }
