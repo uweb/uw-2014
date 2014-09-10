305c940bfaf5b218cfba665735867afac85a11f4
Dane Odekirk <dizzzy@uw.edu>
2014-09-10 10:04:25 -0700

    Big hero template changed.

--- templates/template-big-hero.php
+++ templates/template-big-hero.php
@@ -2,8 +2,49 @@
 /*
 Template Name: Big Hero
  */
-get_header();
 ?>
 
-<div class="uw-hero-image large"></div>
-<?php include(get_stylesheet_directory() . '/templates/template-common.php'); ?>
+<?php get_header(); ?>
+
+<div class="uw-hero-image"></div>
+
+<div class="container uw-body">
+
+  <div class="row">
+
+    <div class="col-md-12 uw-content" role='main'>
+
+      <a href="<?php echo home_url('/'); ?>" title="<?php echo esc_attr( get_bloginfo() ) ?>"><h2 class="uw-site-title"><?php bloginfo(); ?></h2></a>
+
+      <?php get_template_part( 'breadcrumbs' ); ?>
+
+      <div class="uw-body-copy">
+
+        <?php
+          // Start the Loop.
+          while ( have_posts() ) : the_post();
+
+            /*
+             * Include the post format-specific template for the content. If you want to
+             * use this in a child theme, then include a file called called content-___.php
+             * (where ___ is the post format) and that will be used instead.
+             */
+            get_template_part( 'content', 'page' );
+
+            // If comments are open or we have at least one comment, load up the comment template.
+            if ( comments_open() || get_comments_number() ) {
+              comments_template();
+            }
+
+          endwhile;
+        ?>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
+
+<?php get_footer(); ?>
