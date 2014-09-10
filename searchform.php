3cdacdc16bc2f816f65d67724ef1f5784decb45c
Dane Odekirk <dizzzy@uw.edu>
2014-09-10 15:13:16 -0700

    Searchform action added.

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" id="searchform" class="searchform" action="http://128.208.132.94/cms/news/">
+<form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url('/') ?>">
 	<div>
 		<label class="screen-reader-text" for="s">Search for:</label>
 		<input type="text" value="" name="s" id="s" placeholder="Search for:">
