334ca282414aab5d8418e423447cf53ae25d6be5
Dane Odekirk <dizzzy@uw.edu>
2014-09-12 10:47:10 -0700

    Adding site title.

--- header.php
+++ header.php
@@ -6,8 +6,8 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
+        <title> <?php wp_title(' | ',TRUE,'right'); bloginfo('name'); ?> </title>
+        <meta name="description" content="<?php bloginfo('description', 'display'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
