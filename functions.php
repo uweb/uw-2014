eedfca18e3ca64db224e225537b9eb60f08c8208
Dane Odekirk <dizzzy@uw.edu>
2014-09-12 16:12:17 -0700

    Replaced custom page template dropdown with radio buttons.

--- functions.php
+++ functions.php
@@ -8,38 +8,16 @@ if (!isset($UW)){
     $UW = new UW();
 }
 
-// require( get_template_directory() . '/inc/override_page_attributes_meta.php');
-
-// Custom shivs to help Wordpress and some plugins with our server architecture
-// require( get_template_directory() . '/shivs/remove-http-cms.php' );
-//require( get_template_directory() . '/shivs/plugin-hooks.php' );
-
-// Adds a media credit to the media library items
-//require( get_template_directory() . '/inc/replace-media.php' ); // [TODO] could be plugin
+require( get_template_directory() . '/setup/class.uw-page-attributes-meta-box.php');
 
 // All of the UW Widgets TODO: make a dubspack jetpack style thing
-//require( get_template_directory() . '/inc/custom-widgets.php' );
+// require( get_template_directory() . '/inc/custom-widgets.php' );
 
 //require( get_template_directory() . '/inc/custom-gallery.php' );
 //require( get_template_directory() . '/inc/custom-settings.php' ); // [TODO] rename/move?
 
 // All of the UW Shortcodes
 //require( get_template_directory() . '/inc/custom-shortcodes.php' );
-
-// [TODO] check to see if this is still necessary
-//require( get_template_directory() . '/inc/custom-embeds.php' );
-
-// Main dropdown menu walker
-//require( get_template_directory() . '/inc/dropdown-walker.php' );
-
-// Custom Ajax actions
-//require( get_template_directory() . '/inc/custom-ajax-actions.php' );
-
-// Adds functioning enclosure to rss2
-//require( get_template_directory() . '/inc/prep-uw-rss2.php' );
-
-require( get_template_directory() . '/inc/template-functions.php' );
-
 
 // temporary for widgets
 foreach (glob( get_template_directory() . "/widgets/*.php") as $filename)
--- setup/class.uw-page-attributes-meta-box.php
+++ setup/class.uw-page-attributes-meta-box.php
@@ -1,113 +1,106 @@
 <?php
 
-/**
- * Deletes the normal page attributes meta box on pages and adds
- * one that we can manipulate the options on and HTML of
- */
-function add_uw_page_attributes() 
+class UW_Page_Attributes_Meta_Box
 {
-    $post = get_post();
-    $post_type = get_post_type($post);
-    if ( post_type_supports($post_type, 'page-attributes') )
+
+  const ID = 'pageparentdiv';
+  const TITLE = 'Page Attributes';
+  const POSTTYPE = 'page';
+  const POSITION = 'side';
+  const PRIORITY = 'core';
+
+  function __construct()
+  {
+    add_action( 'add_meta_boxes', array( $this, 'replace_meta_box' ) );
+
+  }
+
+  function replace_meta_box()
+  {
+      remove_meta_box( 'pageparentdiv', 'page', 'side');
+	    add_meta_box( 'uwpageparentdiv', 'Page Attributes', array( $this, 'page_attributes_meta_box' ), 'page', 'side', 'core' );
+  }
+
+  function page_attributes_meta_box( $post )
+  {
+
+    $post_type_object = get_post_type_object( $post->post_type );
+
+    if ( $post_type_object->hierarchical )
     {
-        remove_meta_box('pageparentdiv', 'page', 'side');
-	    add_meta_box('uwpageparentdiv', 'Page Attributes', 'uw_page_attributes_meta_box', 'page', 'side', 'core');
-    }
-}
+      $dropdown_args = array(
+        'post_type'        => $post->post_type,
+        'exclude_tree'     => $post->ID,
+        'selected'         => $post->post_parent,
+        'name'             => 'parent_id',
+        'show_option_none' => __('(no parent)'),
+        'sort_column'      => 'menu_order, post_title',
+        'echo'             => 0,
+      );
 
-add_action('add_meta_boxes', 'add_uw_page_attributes', 15, 2);
+          /**
+           * Filter the arguments used to generate a Pages drop-down element.
+           *
+           * @since 3.3.0
+           *
+           * @see wp_dropdown_pages()
+           *
+           * @param array   $dropdown_args Array of arguments used to generate the pages drop-down.
+           * @param WP_Post $post          The current WP_Post object.
+           */
+          $dropdown_args = apply_filters( 'page_attributes_dropdown_pages_args', $dropdown_args, $post );
 
-/**
- * adds a uw_specific page attributes meta box that we control
- *
- * TODO: Clean up that ugly Wordpress HTML
- */
-function uw_page_attributes_meta_box($post)
-{
-	$post_type_object = get_post_type_object($post->post_type);
-	if ( $post_type_object->hierarchical ) {
-		$dropdown_args = array(
-			'post_type'        => $post->post_type,
-			'exclude_tree'     => $post->ID,
-			'selected'         => $post->post_parent,
-			'name'             => 'parent_id',
-			'show_option_none' => __('(no parent)'),
-			'sort_column'      => 'menu_order, post_title',
-			'echo'             => 0,
-		);
+          $pages = wp_dropdown_pages( $dropdown_args );
 
-		/**
-		 * Filter the arguments used to generate a Pages drop-down element.
-		 *
-		 * @since 3.3.0
-		 *
-		 * @see wp_dropdown_pages()
-		 *
-		 * @param array   $dropdown_args Array of arguments used to generate the pages drop-down.
-		 * @param WP_Post $post          The current WP_Post object.
-		 */
-		$dropdown_args = apply_filters( 'page_attributes_dropdown_pages_args', $dropdown_args, $post );
-		$pages = wp_dropdown_pages( $dropdown_args );
-		if ( ! empty($pages) ) {
-        ?>
+          if ( ! empty( $pages ) ) 
+          { ?>
+
             <p><strong><?php _e('Parent') ?></strong></p>
             <label class="screen-reader-text" for="parent_id"><?php _e('Parent') ?></label>
+
             <?php echo $pages; ?>
+
             <?php
-		} // end empty pages check
-	} // end hierarchical check.
-	if ( 'page' == $post->post_type && 0 != count( get_page_templates( $post ) ) ) {
-		$template = !empty($post->page_template) ? $post->page_template : false;
-		?>
-        <p><strong><?php _e('Template') ?></strong></p>
-        <label class="screen-reader-text" for="page_template"><?php _e('Page Template') ?></label>
-        <select name="page_template" id="page_template">
-        <?php
-        //default template select removed because we name the default page.php
-        uw_page_template_dropdown($template);
-        ?>
-        </select>
-        <?php
-	} ?>
+          } // end empty pages check
+    } // end hierarchical check.
+
+    if ( 'page' == $post->post_type && 0 != count( get_page_templates( $post ) ) ) {
+      $template = !empty($post->page_template) ? $post->page_template : false; ?>
+
+
+    <p><strong><?php _e('Template') ?></strong></p>
+
+    <label class="screen-reader-text" for="page_template"><?php _e('Page Template') ?></label>
+
+    <p><input type='radio' value='default' name="page_template" <?php checked( $template, 'default', true ); ?> ><?php _e('Default Template'); ?></input></p>
+
+    <?php $this->page_template_dropdown($template); ?>
+
+    <?php } ?>
+
     <p><strong><?php _e('Order') ?></strong></p>
+
     <p><label class="screen-reader-text" for="menu_order"><?php _e('Order') ?></label><input name="menu_order" type="text" size="4" id="menu_order" value="<?php echo esc_attr($post->menu_order) ?>" /></p>
+
     <p><?php if ( 'page' == $post->post_type ) _e( 'Need help? Use the Help tab in the upper right of your screen.' ); ?></p>
+
     <?php
+  }
+
+  function page_template_dropdown( $default = '' ) {
+
+    $templates = get_page_templates( get_post() );
+
+    ksort( $templates );
+
+    foreach ( array_keys( $templates ) as $template )
+    {
+      $checked = checked( $default, $templates[ $template ], false );
+      echo "<p><input type='radio' name='page_template' value='" . $templates[ $template ] . "' $checked >$template</input></p>";
+    }
+
+  }
+
 }
 
-/**
- * Print out <option> HTML elements for the page templates drop-down.
- *
- * @param string $default Optional. The template file name. Current default Medium Hero.
- */
-function uw_page_template_dropdown( $default = 'Medium Hero' ) {
-    $post = get_post();
-    $front = uw_is_front_page($post);
-    $templates = get_page_templates( $post );
-    if (!$front)
-    {
-        // CHANGE THE FOLLOWING TO CHANGE THE NAME OF THE BIG HERO TEMPLATE
-        unset($templates['Big Hero']);
-    }
-	ksort( $templates );
-	foreach ( array_keys( $templates ) as $template ) {
-		$selected = selected( $default, $templates[ $template ], false );
-		echo "\n\t<option value='" . $templates[ $template ] . "' $selected>$template</option>";
-	}
-}
-
-/**
- * Determine if this is the front page while in the admin back-end
- */
-function uw_is_front_page($post)
-{
-    if ($post->ID == get_option('page_on_front'))
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-?>
+new UW_Page_Attributes_Meta_Box;
