02e8f79553ef819d1bae06f96a38ab5f714c6e5c
Dane Odekirk <dizzzy@uw.edu>
2014-09-18 11:46:46 -0700

    Removing autocomplete from search input.

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url('/') ?>">
 	<div>
 		<label class="screen-reader-text" for="s">Search for:</label>
-		<input type="text" value="" name="s" id="s" placeholder="Search for:">
+		<input type="text" value="" name="s" id="s" placeholder="Search for:" autocomplete="off">
 		<input type="submit" id="searchsubmit" value="Search">
 	</div>
 </form>
