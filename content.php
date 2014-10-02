206cbbce3596b71e00ee4801e2e809a6608e9d92
Dane Odekirk <dizzzy@uw.edu>
2014-10-02 10:23:17 -0700

    Adding post format gallery for posts that require no sidebar.

--- content.php
+++ content.php
@@ -1,4 +1,6 @@
-<h1><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title() ?></a></h1>
+<h1>
+  <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title() ?></a>
+</h1>
 
 <?php
 
