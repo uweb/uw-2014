156f8fa0c8674b82ded549382d9b694cce494511
Dane Odekirk <dizzzy@uw.edu>
2014-10-06 16:14:28 -0700

    Implementing new accessiblity from previous commits to quicklinks.

--- js/uw.quicklinks.js
+++ js/uw.quicklinks.js
@@ -2,13 +2,10 @@
 
 UW.QuickLinks = Backbone.View.extend({
 
-    // todo: the default list and these elements could be put into the php templates
-    container: 'div#uw-container',
-    $little_list_header: $('<h3>Helpful Links</h3>'),
-    $big_list: $('<ul id="big_links"></ul>'),
-    $little_list: $('<ul id="little_list"></ul>'),
     DELAY : 500,
 
+    // todo: the default list and these elements could be put into the php templates
+    container: '#uw-container',
 
     template : '<nav id="quicklinks" role="navigation" aria-label="quick links" aria-hidden="true">' +
                         '<ul id="big-links">' +
@@ -32,19 +29,23 @@ UW.QuickLinks = Backbone.View.extend({
                                 '<% } %>' +
                             '<% }) %>' +
                         '</ul>' +
-                        '<button class="close_quicklinks" style="position:absolute;left:-10000px;" tabindex="-1">close quicklinks</button>' +
                     '</nav>',
 
     events: {
        'click'           : 'animate',
        'touchstart'   : 'animate',
        'keyup'         : 'animate',
-       'blur button' : 'loop'
+       'blur button' : 'loop',
+       // 'keydown button' : 'testShiftKey'
     },
 
+    // testShiftKey : function(e)
+    // {
+    //     console.log(e, e.shiftKey)
+    // },
+
     initialize: function ( options ) {
-        this.$button = this.$el.find('button')
-        _.bindAll( this, 'render', 'accessible', 'loop', 'close_quicklinks', 'add_events', 'check_focus'  );
+        _.bindAll( this, 'render', 'animate', 'accessible', 'loop'  );
         this.links = new UW.QuickLinks.Collection( options )
         this.links.on( 'sync', this.render )
     },
@@ -52,14 +53,13 @@ UW.QuickLinks = Backbone.View.extend({
     render : function(  )
     {
         this.quicklinks = $ ( _.template( this.template, { links : this.links.toJSON() }) )
-        this.make_drawer();
+        this.makeDrawer()
         this.$container.prepend( this.quicklinks )
-        this.$button.attr('aria-controls', 'quicklinks').attr('aria-owns', 'quicklinks');
-        this.$drawer = this.$container.find('nav#quicklinks');
-        this.add_events();
+        this.$el.attr( 'aria-controls', 'quicklinks' ).attr( 'aria-owns', 'quicklinks' )
+        UW.$body.on( 'keyup', '#quicklinks a', this.animate )
     },
 
-    make_drawer: function () {
+    makeDrawer: function () {
         var $shortcuts = UW.$body.find('a.screen-reader-shortcut').detach();
         UW.$body.children().not('#wpadminbar').not('script')
             .wrapAll('<div id="uw-container">')
@@ -68,75 +68,14 @@ UW.QuickLinks = Backbone.View.extend({
         this.$container.prepend($shortcuts);
     },
 
-    close_quicklinks: function (event) {
-        if (event.type == 'keyup'){
-            if (event.keyCode == 27) {
-                this.$button.focus();
-                this.animate(event);
-            }
-        }
-        else if (this.$container.hasClass('open') && (event.target.parentElement != this.el)) {
-            this.animate(event);
-        }
-    },
-
-    add_links: function () {
-        _.each( this.collection.models, this.append_menu_item )
-    },
-
-    append_menu_item: function ( link ) {
-        if ( link.get('classes') )
-            this.$big_list.append( _.template( this.template, link.toJSON() ) )
-         else
-            this.$little_list.append( _.template( this.template, link.toJSON() ) )
-    },
-
-    //add_lists : function () {
-    //    if (this.$little_list.find('li').length > 0) {
-    //        this.$drawer.prepend(this.$little_list);
-    //        this.$drawer.prepend(this.$little_list_header);
-    //    }
-    //    if (this.$big_list.find('li').length > 0) {
-    //        this.$drawer.prepend(this.$big_list);
-    //    }
-    //    this.add_events();
-    //    this.$container.prepend(this.$drawer);
-    //},
-
-    add_events: function () {
-        this.$links = this.$drawer.find('a');
-        $('#uw-container-inner').on( {
-            'click': this.close_quicklinks
-        });
-        this.$drawer.find('button.close_quicklinks').on({
-            'click': this.close_quicklinks
-        });
-        this.$links.on( {'keyup': this.close_quicklinks});
-        var self = this;
-        this.$links.blur(function() {
-            _.defer(self.check_focus);
-        });
-    },
-    
-    check_focus: function (event) {
-        if (($.inArray(document.activeElement, this.$links) != -1) || document.activeElement == this.$button[0]){
-            return;
-        }
-        else {
-            this.$button.focus();
-        }
-    },
-
     animate: function ( e ) {
         e.preventDefault();
 
-        if ( e.keyCode && (e.keyCode != 27 )){
+        if ( e.keyCode && e.keyCode != 27 )
+        {
+            if ( e.keyCode && e.keyCode != 13 ||
+                e.keyCode && e.keyCode != 32 )
             return false;
-        }
-        else if ( e.keyCode && e.keyCode == 27){
-            if (!this.open){
-                return false;
-            }
         }
 
         this.$container.toggleClass('open')
@@ -147,19 +86,20 @@ UW.QuickLinks = Backbone.View.extend({
         _.delay( this.accessible, this.open ? this.DELAY : 0 )
     },
 
+    // todo : cache the uw-container-inner and screen-reader
     accessible : function (argument)
     {
         this.$el.find('button').attr( 'aria-expanded', this.open )
         this.quicklinks.attr('aria-hidden',  ( ! this.open ).toString() )
-        if ( this.open ){
+        if ( this.open ) {
             this.quicklinks.find('a').attr( 'tabindex', 0 ).first().focus()
-            $('#uw-container-inner').attr('aria-hidden', true);
-            $('.screen-reader-shortcut').attr('aria-hidden', true)
-        }
-        else{
+           $('#uw-container-inner').attr('aria-hidden', true);
+           $('.screen-reader-shortcut').attr('aria-hidden', true)
+        } else {
             this.quicklinks.find('a').attr( 'tabindex', -1 )
-            $('#uw-container-inner').attr('aria-hidden', false);
-            $('.screen-reader-shortcut').attr('aria-hidden', false);
+            this.$el.find('button').focus()
+           $('#uw-container-inner').attr('aria-hidden', false);
+           $('.screen-reader-shortcut').attr('aria-hidden', false);
         }
     },
 
