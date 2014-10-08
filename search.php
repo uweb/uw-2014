9a7307ea5be8bfacaaa90fe7ca91dcd6c68650dd
Dane Odekirk <dizzzy@uw.edu>
2014-10-08 15:25:07 -0700

    Adding search results template.

---
+++ search.php
@@ -0,0 +1,43 @@
+<?php get_header(); ?>
+
+<?php get_template_part( 'header', 'image' ); ?>
+
+<div class="container uw-body">
+
+  <div class="row">
+
+    <div <?php uw_content_class(); ?> role='main'>
+
+      <a href="<?php echo home_url('/'); ?>" title="<?php echo esc_attr( get_bloginfo() ) ?>"><h2 class="uw-site-title"><?php bloginfo(); ?></h2></a>
+
+      <?php get_template_part( 'breadcrumbs' ); ?>
+
+      <div id='main_content' class="uw-body-copy">
+
+        <?php
+          // Start the Loop.
+          while ( have_posts() ) : the_post();
+
+            /*
+             * Include the post format-specific template for the content. If you want to
+             * use this in a child theme, then include a file called called content-___.php
+             * (where ___ is the post format) and that will be used instead.
+             */
+            get_template_part( 'content', 'archive' );
+
+          endwhile;
+        ?>
+
+        <?php posts_nav_link(' '); ?>
+
+      </div>
+
+    </div>
+
+    <?php get_sidebar() ?>
+
+  </div>
+
+</div>
+
+<?php get_footer(); ?>
