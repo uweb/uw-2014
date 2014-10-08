fd94ee864d386c59ee44e93ef443c4090990a146
Dane Odekirk <dizzzy@uw.edu>
2014-10-08 13:01:33 -0700

    Adding archive template.

---
+++ content-archive.php
@@ -0,0 +1,11 @@
+<h1>
+  <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title() ?></a>
+</h1>
+
+<?php
+  if ( ! is_home() && ! is_search() && ! is_archive() ) :
+    uw_mobile_menu();
+  endif;
+?>
+
+<?php the_excerpt(); ?>
