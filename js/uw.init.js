9338789355c3e209d8b468a0e6e20b02781a8d2a
Jon Swanson <jswan2@uw.edu>
2014-10-14 15:17:00 -0700

    Revert "Merge branch 'master' of ssh://cmsdev/data/marketing/git/uw-2014"
    This reverts commit 3073628d3d1bb1c0fcb0fe2d789096841c1e074f, reversing
    changes made to f84010ad3c7e1d7d3f77504363cce2d3c9137fce.

--- js/uw.init.js
+++ js/uw.init.js
@@ -17,8 +17,7 @@ UW.elements = {
 
 }
 
-UW.baseUrl = uw.baseUrl || Backbone.history.location.origin + '/' +
-                    _.first( _.compact( Backbone.history.location.pathname.split('/') ) ) + '/';
+UW.baseUrl = uw_wordpress_vars.site_url;
 
 UW.sources = {
   quicklinks : UW.baseUrl + 'wp-admin/admin-ajax.php?action=quicklinks',
