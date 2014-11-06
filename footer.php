b6be8ab6e425d384d15635a158b2bf735986581d
Jon Swanson <jswan2@uw.edu>
2014-11-05 21:06:40 -0800

    changed myuw footer link to something that works (non-secure)

--- footer.php
+++ footer.php
@@ -26,7 +26,7 @@
                 <li><a href="http://uw.edu/home/siteinfo/form">Contact Us</a></li>
                 <li><a href="http://www.washington.edu/jobs">Jobs</a></li>
                 <li><a href="http://www.washington.edu/safety">Campus Safety</a></li>
-                <li><a href="https://myuw.washington.edu/">My UW</a></li>
+                <li><a href="http://myuw.washington.edu/">My UW</a></li>
                 <li><a href="http://www.washington.edu/admin/rules/wac/rulesindex.html">Rules Docket</a></li>
                 <li><a href="http://www.washington.edu/online/privacy">Privacy</a></li>
                 <li><a href="http://www.washington.edu/online/terms">Terms</a></li>
