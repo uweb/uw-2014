92d1a9a2db15db3a56e0bb5be41b325c2b9d9e41
bpe <ben@benjaminerickson.com>
2014-11-21 12:14:08 -0800

    fixed static call in widget admin area

--- widgets/class.uw-widget-visibility.php
+++ widgets/class.uw-widget-visibility.php
@@ -25,7 +25,7 @@ class UW_Widget_Conditions
     }
   }
 
-  function widget_admin_setup()
+  public static function widget_admin_setup()
   {
     wp_enqueue_style( 'widget-conditions', get_template_directory_uri() .  '/assets/admin/css/uw.widget-conditions.css' );
     wp_enqueue_script( 'widget-conditions', get_template_directory_uri() . '/assets/admin/js/uw.widget-conditions.js' , array( 'jquery', 'jquery-ui-core' ), 20140422, true );
