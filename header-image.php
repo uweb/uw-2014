72230a935c1b8af2960f036102bfec4ed5099c1a
Dane Odekirk <dizzzy@uw.edu>
2014-12-31 13:07:50 -0800

    Setting URL scheme for background image.

--- header-image.php
+++ header-image.php
@@ -1,2 +1,2 @@
 
-<div class="uw-hero-image" <?php if ( get_header_image() !== '' )  { ?> style="background-image:url('<?php header_image(); ?>');"<?php } ?> ></div>
+<div class="uw-hero-image" <?php if ( get_header_image() !== '' )  { ?> style="background-image:url('<?php echo set_url_scheme( get_header_image() ); ?>');"<?php } ?> ></div>
