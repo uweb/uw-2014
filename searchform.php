207c1c80382f875b6c68c0ab575936a4bdc8fbd4
Dane Odekirk <dizzzy@uw.edu>
2014-12-31 13:19:57 -0800

    Setting url scheme for the search form.

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url('/') ?>">
+<form role="search" method="get" id="searchform" class="searchform" action="<?php echo set_url_scheme( home_url('/') ) ?>">
 	<div>
 		<label class="screen-reader-text" for="s">Search for:</label>
 		<input type="text" value="" name="s" id="s" placeholder="Search for:" autocomplete="off">
