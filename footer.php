015daffe7fa88a260fddb1e72b5a530cec1ad013
Dane Odekirk <dizzzy@uw.edu>
2015-01-05 10:32:07 -0800

    Adding current year to copyright.

--- footer.php
+++ footer.php
@@ -33,7 +33,7 @@
             </ul>
         </nav>
 
-        <p role="contentinfo">&copy;	 2014 University of Washington  |  Seattle, WA</p>
+        <p role="contentinfo">&copy; <?php echo date("Y"); ?> University of Washington  |  Seattle, WA</p>
 
 
     </div>
