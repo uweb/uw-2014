94c2dbd1e7e16634acec31100804e16ca7dc6e90
Ben Erickson <bperick@uw.edu>
2015-01-09 09:51:25 -0800

    fixing baseurl omission

--- js/uw.init.js
+++ js/uw.init.js
@@ -26,7 +26,7 @@ else {
 }
 
 UW.sources = {
-  quicklinks : 'wp-admin/admin-ajax.php?action=quicklinks',
+  quicklinks : UW.baseUrl + 'wp-admin/admin-ajax.php?action=quicklinks',
   search     : UW.baseUrl + 'wp-admin/admin-ajax.php'
 }
 
