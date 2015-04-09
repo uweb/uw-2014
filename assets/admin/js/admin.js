cb8c1d731425f1fef3264c30e37c706bdb0cfa76
Dane Odekirk <daneodekirk@gmail.com>
2015-04-09 15:52:56 -0700

    Added animation to anchors to not be covered by the fixed nav.

--- assets/admin/js/admin.js
+++ assets/admin/js/admin.js
@@ -4,11 +4,20 @@ jQuery(document).ready(function($){
 
     if ( $('#uw-documentation').length )
     {
+
       var text = $('#markdown').text()
           , converter = new Showdown.converter()
           , html = converter.makeHtml( text )
 
       $('#uw-documentation').html( html )
+
+      $('a').on('click', function() {
+        var href = $(this).attr('href')
+        if ( _.first( href )  === '#') {
+          $( 'html, body' ).animate( {'scrollTop': $(href).position().top}, 700 )
+          return
+        }
+      })
     }
 
 
