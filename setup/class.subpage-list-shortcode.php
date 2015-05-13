91ba3185ad92e979e41ea4c374f9eaec9714c8db
Jon Swanson <jswan2@uw.edu>
2015-05-13 15:21:13 -0700

    added boxtile logic to subpage list shortcode

--- setup/class.subpage-list-shortcode.php
+++ setup/class.subpage-list-shortcode.php
@@ -33,6 +33,7 @@ class UW_SubpageList
 
         if (!empty($subpages)){
           $output = '';
+          $tiles = 0;
           foreach ($subpages as $page){
             
             $permalink = get_post_permalink($page->ID);
@@ -48,8 +49,28 @@ class UW_SubpageList
               }
             }
             else {
-              //tilebox logic coming 
+              if ($tiles == 0){
+                $output = $output . "<div class='box-outer'><div class='box two'>";
+              }
+              $output = $output . "<div class='tile'>";
+              $output = $output . sprintf("<div>%s</div>", get_the_post_thumbnail($page->ID, 'half'));
+              $output = $output . sprintf("<h3><a href='%s'>%s</a></h3>", $permalink, $page->post_title);
+              if (get_option('show_byline_on_posts')){
+                $output = $output . sprintf("<div class='author-info'><p class='author-desc'><small>%s</small></p></div>", get_the_author_meta('display_name', $page->post_author));
+              }
+              $output = $output . sprintf('<p>%s</p>', $page->post_excerpt);
+              $output = $output . "</div>";
+              $tiles = $tiles + 1;
+              if ($tiles == 2){
+                $output = $output . "</div></div>";
+                $tiles = 0;
+              }
             }
+          }
+
+          if ($tiles == 1){
+            //we've been incrementing tiles, so tilebox is active, but we are missing a blank tile and closing tags
+            $output = $output . "<div class='tile empty'></div></div></div>"; 
           }
         }
 
