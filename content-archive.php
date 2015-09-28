d3c550ff6abf2eb7343853308385ede03310c3e7
Nick Winkelbauer <nwinkelb@uw.edu>
2015-09-28 15:30:12 -0700

    Feature Added: post archives now display feature images

--- content-archive.php
+++ content-archive.php
@@ -14,6 +14,8 @@ endif;
   if ( ! is_home() && ! is_search() && ! is_archive() ) :
     uw_mobile_menu();
   endif;
+ if ( has_post_thumbnail() ) :
+ 	the_post_thumbnail();
+ endif;
 ?>
-
 <?php the_excerpt(); ?>
