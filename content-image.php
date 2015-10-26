a66b7f2c9b345ba1813fea613246b378297b7dbd
Nick Winkelbauer <nwinkelb@uw.edu>
2015-10-26 15:09:03 -0700

    Feature: adds photo credit to image pages

--- content-image.php
+++ content-image.php
@@ -5,6 +5,10 @@
   <h1 class="entry-title"><?php the_title(); ?></h1>
 
   <?php echo get_the_excerpt(); ?>
+  <?php 
+  	$credit = get_post_meta( $post->ID, '_media_credit', true);
+  	if ($credit) echo "<br> Photo Credit: " . $credit; 
+  ?>
 
   <div>
     <a href="<?php echo wp_get_attachment_url(get_the_ID());?>" title="<?php the_title(); ?>" target="_blank" download="<?php the_title() ?>">Download</a>
