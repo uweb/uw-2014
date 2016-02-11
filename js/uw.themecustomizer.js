0e2ddc7b999165a7efe8b60b88f5c06579b99fbc
Nick Winkelbauer <nwinkelb@uw.edu>
2016-02-11 14:00:32 -0800

    Bug fix: not all header images in customizer use the text 'cropped' so switched to generic

--- js/uw.themecustomizer.js
+++ js/uw.themecustomizer.js
@@ -6,7 +6,7 @@ jQuery( document ).ready( function($) {
 	
 	// HEADERS TO HTTPS
 	$( "#accordion-section-header_image img" ).each(function() { 
-		if (this.src.includes("cropped") && window.location.href.includes(".edu")) {
+		if (!this.src.includes("https:") && window.location.href.includes(".edu")) {
 			this.src = this.src.replace( "http:" , "https:" );
 		}
 	});
