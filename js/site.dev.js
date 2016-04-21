17f521c61e84eb584a03a26c93feb53053d20792
Kilian <kilianf@uw.edu>
2016-04-20 17:40:36 -0700

    Switch over Uni Sans to the 2014 theme and updated quicklinks URL

--- js/site.dev.js
+++ js/site.dev.js
@@ -10992,9 +10992,7 @@ UW.wpinstance = function(){
 
 UW.sources = {
   // Note: style_dir is a variable created by the Wordpress' wp_localize_script in class.uw-scripts.php
-  quicklinks : function(){
-    return typeof(style_dir) !== 'undefined' ? style_dir + '/wp-admin/admin-ajax.php?action=quicklinks' : UW.getBaseUrl() + 'wp-admin/admin-ajax.php?action=quicklinks'
-  },
+  quicklinks : typeof(style_dir) !== 'undefined' ? style_dir + '/wp-admin/admin-ajax.php?action=quicklinks' : UW.getBaseUrl() + 'wp-admin/admin-ajax.php?action=quicklinks',
   search     : UW.getBaseUrl() + 'wp-admin/admin-ajax.php'
 }
 
