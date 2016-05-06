defdf356be9f7957341844cacfe609990b75ab8d
Nick Winkelbauer <nwinkelb@uw.edu>
2016-05-06 13:51:00 -0700

    All menus use dropdown from front

--- menu-mobile.php
+++ menu-mobile.php
@@ -1,7 +1,9 @@
 <?php
-if (( is_search() || is_archive() || is_front_page() )){
-  uw_mobile_front_page_menu();
-} else {
-  uw_mobile_menu();
-}
+// if (( is_search() || is_archive() || is_front_page() )){
+//   uw_mobile_front_page_menu();
+// } else {
+//   uw_mobile_menu();
+// }
+// 
+uw_mobile_front_page_menu();
 ?>
