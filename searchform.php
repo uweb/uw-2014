2330fd79aace33868b954abb76070de2726c34a3
Nick Winkelbauer <nwinkelb@uw.edu>
2016-05-16 14:55:04 -0700

    fix

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <?php $url = is_multisite() ? get_site_url(1, '/') : site_url('/'); ?>
-<form role="search" method="get" id="searchform" class="searchform" action="<?php echo set_url_scheme(  ) ?>">
+<form role="search" method="get" id="searchform" class="searchform" action="<?php echo set_url_scheme( $url ) ?>">
 	<div>
 		<label class="screen-reader-text" for="s">Search for:</label>
 		<input type="text" value="" name="s" id="s" placeholder="Search for:" autocomplete="off">
