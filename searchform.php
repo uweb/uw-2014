22e69ed1e04717f2a8c7dbb9d6692390d0196ea3
Nick Winkelbauer <nwinkelb@uw.edu>
2016-05-16 14:52:32 -0700

    fix 404 search

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
-<?php $url = is_multisite() ? network_site_url('/') : site_url('/'); ?>
-<form role="search" method="get" id="searchform" class="searchform" action="<?php echo set_url_scheme( $url ) ?>">
+<?php $url = is_multisite() ? get_site_url(1, '/') : site_url('/'); ?>
+<form role="search" method="get" id="searchform" class="searchform" action="<?php echo set_url_scheme(  ) ?>">
 	<div>
 		<label class="screen-reader-text" for="s">Search for:</label>
 		<input type="text" value="" name="s" id="s" placeholder="Search for:" autocomplete="off">
