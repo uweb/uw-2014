225c1aad28a158fc3f4669c271af262d695563d5
Nick Winkelbauer <nwinkelb@uw.edu>
2016-05-19 15:12:24 -0700

    test

--- searchform.php
+++ searchform.php
@@ -2,7 +2,7 @@
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo set_url_scheme( $url ) ?>">
 	<div>
 		<label class="screen-reader-text" for="s">Search for:</label>
-		<input type="text" value="" name="s" id="s" placeholder="Search for: <?php echo get_current_blog_id(); ?>" autocomplete="off">
+		<input type="text" value="" name="s" id="s" placeholder="Search for:" autocomplete="off">
 		<input type="submit" id="searchsubmit" value="Search">
 	</div>
 </form>
