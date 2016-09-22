bc7f5ea38174eb4c446852003618245a28f74dab
todd-uams <todd@uams.edu>
2016-09-22 15:34:20 -0500

    Updated Footer

--- footer.php
+++ footer.php
@@ -1,39 +1,39 @@
     <div role="contentinfo" class="uams-footer">
 
-        <a href="http://www.washington.edu" class="footer-wordmark">University of Washington</a>
-
-        <a href="http://www.washington.edu/boundless/"><h3 class="be-boundless">Be boundless</h3></a>
+        <a href="http://www.uams.edu" class="footer-wordmark">University of Washington</a>
 
         <h4>Connect with us:</h4>
 
         <nav role="navigation" aria-label="social networking">
             <ul class="footer-social">
-                <li><a class="facebook" href="http://www.facebook.com/UofWA">Facebook</a></li>
-                <li><a class="twitter" href="http://twitter.com/UW">Twitter</a></li>
-                <li><a class="instagram" href="http://instagram.com/uofwa">Instagram</a></li>
-                <li><a class="tumblr" href="http://uofwa.tumblr.com/">Tumblr</a></li>
-                <li><a class="youtube" href="http://www.youtube.com/user/uwhuskies">YouTube</a></li>
-                <li><a class="linkedin" href="http://www.linkedin.com/company/university-of-washington">LinkedIn</a></li>
-                <li><a class="pinterest" href="http://www.pinterest.com/uofwa/">Pinterest</a></li>
-                <li><a class="vine" href="https://vine.co/uofwa">Vine</a></li>
-                <li><a class="google" href="https://plus.google.com/+universityofwashington/posts">Google+</a></li>
+                <li><a class="facebook" href="http://facebook.com/uamshealth">Facebook</a></li>
+                <li><a class="twitter" href="http://twitter.com/uamshealth">Twitter</a></li>
+                <li><a class="instagram" href="http://instagram.com/uamshealth">Instagram</a></li>
+<!--                 <li><a class="tumblr" href="http://uofwa.tumblr.com/">Tumblr</a></li> -->
+                <li><a class="youtube" href="https://www.youtube.com/user/UAMSHealth">YouTube</a></li>
+                <li><a class="linkedin" href="https://www.linkedin.com/company/uams">LinkedIn</a></li>
+                <li><a class="pinterest" href="http://pinterest.com/uamshealth">Pinterest</a></li>
+<!--                 <li><a class="vine" href="https://vine.co/uofwa">Vine</a></li> -->
+<!--                 <li><a class="google" href="https://plus.google.com/+universityofwashington/posts">Google+</a></li> -->
             </ul>
         </nav>
 
         <nav role="navigation" aria-label="footer links">
             <ul class="footer-links">
-                <li><a href="http://www.uw.edu/accessibility">Accessibility</a></li>
-                <li><a href="http://uw.edu/home/siteinfo/form">Contact Us</a></li>
-                <li><a href="http://www.washington.edu/jobs">Jobs</a></li>
+<!--                 <li><a href="http://www.uw.edu/accessibility">Accessibility</a></li> -->
+                <li><a href="http://uamshealth.com/disclaimer/">Disclaimer</a></li>
+                <li><a href="http://jobs.uams.edu">Jobs</a></li>
+<!--
                 <li><a href="http://www.washington.edu/safety">Campus Safety</a></li>
                 <li><a href="http://myuw.washington.edu/">My UW</a></li>
-                <li><a href="http://www.washington.edu/rules/wac">Rules Docket</a></li>
-                <li><a href="http://www.washington.edu/online/privacy/">Privacy</a></li>
-                <li><a href="http://www.washington.edu/online/terms/">Terms</a></li>
+-->
+                <li><a href="http://uamshealth.com/privacy/#legal">Copyright Statement</a></li>
+                <li><a href="http://uamshealth.com/privacy/">Privacy</a></li>
+                <li><a href="http://uamshealth.com/termsofuse/">Terms</a></li>
             </ul>
         </nav>
 
-        <p>&copy; <?php echo date("Y"); ?> University of Washington  |  Seattle, WA</p>
+        <p>&copy; <?php echo date("Y"); ?> University of Arkansas for Medical Sciences  |  Little Rock, AR</p>
 
     </div>
 
