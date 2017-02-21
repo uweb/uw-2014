3814ccd1fd17551ba6d3bbec4718342e457882a5
Nick Winkelbauer <nwinkelb@uw.edu>
2017-02-21 11:13:41 -0800

    quick style rss feed button

--- widgets/class.uw-recent-posts.php
+++ widgets/class.uw-recent-posts.php
@@ -43,22 +43,37 @@ class UW_Recent_Posts extends WP_Widget
 
     <h2><?php  echo $title; ?>
 
-      <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-         width="25.526px" height="24.609px" viewBox="0 0 25.526 24.609" enable-background="new 0 0 25.526 24.609" xml:space="preserve">
-      <g>
-        <g>
-          <path fill="#20A2ED" d="M12.763,0c-6.617,0-12,5.383-12,12c0,6.617,5.383,12,12,12s12-5.383,12-12C24.763,5.383,19.38,0,12.763,0z
-             M12.763,21.818c-5.414,0-9.818-4.405-9.818-9.818s4.404-9.818,9.818-9.818S22.582,6.586,22.582,12S18.177,21.818,12.763,21.818z
-             M13.854,7.638h-2.182v6.545h2.182v-0.001h3.272V12h-3.272V7.638z"/>
-        </g>
-      </g>
-      </svg>
-
       <?php if ( $feed )  : ?>
-        <a class="feed" href="<?php echo bloginfo('rss2_url'); ?>">feed</a>
+        <a class="feed" href="<?php echo bloginfo('rss2_url'); ?>" >
+          <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+             width="22.564px" height="22.948px" viewBox="0 0 22.564 22.948" enable-background="new 0 0 22.564 22.948" xml:space="preserve">
+          <g>
+            <path fill="none" stroke="#4C2F83" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" d="M2.239,12.029
+              c4.428,0.041,8.05,3.663,8.091,8.09"/>
+            <path fill="none" stroke="#4C2F83" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" d="M2.194,7.197
+              c7.095,0.067,12.9,5.873,12.968,12.968"/>
+            <path fill="none" stroke="#4C2F83" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" d="M2.145,2.036
+              c9.946,0.094,18.084,8.231,18.177,18.177"/>
+          </g>
+          <circle fill="#4C2F83" cx="3.16" cy="19.198" r="2.015"/>
+          </svg>
+        </a>
+      <?php else: ?>
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+           width="25.526px" height="24.609px" viewBox="0 0 25.526 24.609" enable-background="new 0 0 25.526 24.609" xml:space="preserve">
+        <g>
+          <g>
+            <path fill="#20A2ED" d="M12.763,0c-6.617,0-12,5.383-12,12c0,6.617,5.383,12,12,12s12-5.383,12-12C24.763,5.383,19.38,0,12.763,0z
+               M12.763,21.818c-5.414,0-9.818-4.405-9.818-9.818s4.404-9.818,9.818-9.818S22.582,6.586,22.582,12S18.177,21.818,12.763,21.818z
+               M13.854,7.638h-2.182v6.545h2.182v-0.001h3.272V12h-3.272V7.638z"/>
+          </g>
+        </g>
+        </svg>
       <?php endif; ?>
 
     </h2>
+
+      
 
     <ul class="recent-posts">
     <?php foreach ( $recent as $post ) : ?>
@@ -86,6 +101,8 @@ class UW_Recent_Posts extends WP_Widget
     <?php if ( get_option( 'page_for_posts' ) && $more )  : ?>
       <a class="more" href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>">More</a>
     <?php endif; ?>
+
+
 
     <?php echo $after_widget; ?>
 
