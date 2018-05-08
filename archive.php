d389c638c5ad585ad91de3a409d208cca294e705
Jessie Kuo <kuoy2@uw.edu>
2018-05-08 16:00:55 -0700

    prioritize sticky posts in categorize pages and blogroll shortcode

--- archive.php
+++ archive.php
@@ -19,18 +19,52 @@
       <h1><?php echo single_cat_title( '', false ); ?></h1><hr>
 
         <?php
-          // Start the Loop.
-          while ( have_posts() ) : the_post();
+
+
+/* ------- Jessie's function : Prioritize Sticky posts in categorize pages------------ */
+          $sticky = get_option( 'sticky_posts' );
+          $args_sticky = array(
+              'showposts' => -1,
+              'post__in' => $sticky
+          );
+
+          $args_nonsticky = array(
+             'posts_per_page' => -1,
+             'post__not_in' => $sticky
+          );
+
+          $the_query_sticky = new WP_Query($args_sticky);
+          $the_query_nonsticky = new WP_Query($args_nonsticky);
+
+          // reorder posts so that sticky posts comes first
+          if ($sticky[0]) {
+              while ($the_query_sticky -> have_posts()) : $the_query_sticky -> the_post();
+                  get_template_part( 'content', 'archive' );
+              endwhile;
+          }
+
+          while ($the_query_nonsticky -> have_posts()) : $the_query_nonsticky -> the_post();
+              get_template_part( 'content', 'archive' );
+          endwhile;
+/* ------- Jessie's function Ends Here ----------------------------------------------- */
+
+
+          /*
+           * This is the original function
+           */
+
+          //while ( have_posts() ) : the_post();
 
             /*
              * Include the post format-specific template for the content. If you want to
              * use this in a child theme, then include a file called called content-___.php
              * (where ___ is the post format) and that will be used instead.
              */
-            get_template_part( 'content', 'archive' );
+            //get_template_part( 'content', 'archive' );
+
+          //endwhile;
 
 
-          endwhile;
         ?>
         </br>
         <?php posts_nav_link(' ', 'Previous page', 'Next page'); ?>
--- widgets/class.uw-blogroll.php
+++ widgets/class.uw-blogroll.php
@@ -82,7 +82,44 @@ class UW_Blogroll extends WP_Widget
     if ( !array_key_exists('numberposts', $params ) )
       $params['numberposts'] = $params['number'];
 
-    $posts = get_posts( $params );
+
+/*  Jessie's function : Prioritize Sticky posts in blogroll shortcode------------------------------ */
+    function get_posts_sticky_first ( $args = null ) {
+        $defaults = array(
+            'numberposts' => 5,
+            'category' => 0, 'orderby' => 'date',
+            'order' => 'DESC', 'include' => array(),
+            'exclude' => array(), 'meta_key' => '',
+            'meta_value' =>'', 'post_type' => 'post',
+            'suppress_filters' => true
+        );
+
+        $r = wp_parse_args( $args, $defaults );
+        if ( empty( $r['post_status'] ) )
+            $r['post_status'] = ( 'attachment' == $r['post_type'] ) ? 'inherit' : 'publish';
+        if ( ! empty($r['numberposts']) && empty($r['posts_per_page']) )
+            $r['posts_per_page'] = $r['numberposts'];
+        if ( ! empty($r['category']) )
+            $r['cat'] = $r['category'];
+        if ( ! empty($r['include']) ) {
+            $incposts = wp_parse_id_list( $r['include'] );
+            $r['posts_per_page'] = count($incposts);  // only the number of posts included
+            $r['post__in'] = $incposts;
+        } elseif ( ! empty($r['exclude']) )
+            $r['post__not_in'] = wp_parse_id_list( $r['exclude'] );
+
+        //$r['ignore_sticky_posts'] = true;
+        $r['no_found_rows'] = true;
+
+        $get_posts = new WP_Query;
+        return $get_posts->query($r);
+    }
+
+    $posts = get_posts_sticky_first( $params );
+/*  Jessie's function Ends --------------------------------------------------- */
+
+
+    //$posts = get_posts( $params );
 
     $params = (object) $params;
     $mini = $params->mini;
