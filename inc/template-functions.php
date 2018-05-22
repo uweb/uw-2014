924e512a16c3d9cf01ff4627373678a92044af53
Jessie Kuo <kuoy2@uw.edu>
2018-05-22 15:38:55 -0700

    solved footer issue in blogroll page by moving sticky prioritizing function into template-functions.php file

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -364,3 +364,39 @@ if ( !function_exists('uw_site_title')):
     }
 
 endif;
+
+/*  Jessie's function : Prioritize Sticky posts in blogroll shortcode------------------------------ */
+if ( ! function_exists('get_posts_sticky_first') ) :
+
+  function get_posts_sticky_first ( $args = null ) {
+      $defaults = array(
+          'numberposts' => 5,
+          'category' => 0, 'orderby' => 'date',
+          'order' => 'DESC', 'include' => array(),
+          'exclude' => array(), 'meta_key' => '',
+          'meta_value' =>'', 'post_type' => 'post',
+          'suppress_filters' => true
+      );
+
+      $r = wp_parse_args( $args, $defaults );
+      if ( empty( $r['post_status'] ) )
+          $r['post_status'] = ( 'attachment' == $r['post_type'] ) ? 'inherit' : 'publish';
+      if ( ! empty($r['numberposts']) && empty($r['posts_per_page']) )
+          $r['posts_per_page'] = $r['numberposts'];
+      if ( ! empty($r['category']) )
+          $r['cat'] = $r['category'];
+      if ( ! empty($r['include']) ) {
+          $incposts = wp_parse_id_list( $r['include'] );
+          $r['posts_per_page'] = count($incposts);  // only the number of posts included
+          $r['post__in'] = $incposts;
+      } elseif ( ! empty($r['exclude']) )
+          $r['post__not_in'] = wp_parse_id_list( $r['exclude'] );
+
+      $r['no_found_rows'] = true;
+
+      $get_posts = new WP_Query;
+      return $get_posts->query($r);
+  }
+
+endif;
+/*  Jessie's function Ends ------------------------------------------------------------------------- */
--- widgets/class.uw-blogroll.php
+++ widgets/class.uw-blogroll.php
@@ -83,38 +83,7 @@ class UW_Blogroll extends WP_Widget
       $params['numberposts'] = $params['number'];
 
 
-/*  Jessie's function : Prioritize Sticky posts in blogroll shortcode------------------------------ */
-    function get_posts_sticky_first ( $args = null ) {
-        $defaults = array(
-            'numberposts' => 5,
-            'category' => 0, 'orderby' => 'date',
-            'order' => 'DESC', 'include' => array(),
-            'exclude' => array(), 'meta_key' => '',
-            'meta_value' =>'', 'post_type' => 'post',
-            'suppress_filters' => true
-        );
-
-        $r = wp_parse_args( $args, $defaults );
-        if ( empty( $r['post_status'] ) )
-            $r['post_status'] = ( 'attachment' == $r['post_type'] ) ? 'inherit' : 'publish';
-        if ( ! empty($r['numberposts']) && empty($r['posts_per_page']) )
-            $r['posts_per_page'] = $r['numberposts'];
-        if ( ! empty($r['category']) )
-            $r['cat'] = $r['category'];
-        if ( ! empty($r['include']) ) {
-            $incposts = wp_parse_id_list( $r['include'] );
-            $r['posts_per_page'] = count($incposts);  // only the number of posts included
-            $r['post__in'] = $incposts;
-        } elseif ( ! empty($r['exclude']) )
-            $r['post__not_in'] = wp_parse_id_list( $r['exclude'] );
-
-        //$r['ignore_sticky_posts'] = true;
-        $r['no_found_rows'] = true;
-
-        $get_posts = new WP_Query;
-        return $get_posts->query($r);
-    }
-
+/*  Calling Jessie's function : Prioritize Sticky posts in blogroll shortcode------------------------------ */
     $posts = get_posts_sticky_first( $params );
 /*  Jessie's function Ends ------------------------------------------------------------------------- */
 
