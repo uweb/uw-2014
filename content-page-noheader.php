8c25d1eec0174c22cb35d4e1e785e032270e97f2
umac10 <umac10@uw.edu>
2018-06-26 10:50:22 -0700

    Fix bug that throws an error when trying to use the text_cut() function. Added the function to the template-functions file

--- content-page-noheader.php
+++ content-page-noheader.php
@@ -1,29 +1,12 @@
-<?php
-	function text_cut($text, $length = 27, $dots = true) {   
-		global $post;
-		$parent = get_post($post->post_parent);
-		$text = $parent->post_title;
-		$text = trim(preg_replace('#[\s\n\r\t]{2,}#', ' ', $text));    
-		$text_temp = $text;    
-		while (substr($text, $length, 1) != " ") {
-			$length--; 
-			if ($length > strlen($text)) { break; }
-		}     
-		$text = substr($text, 0, $length);     
-		return $text . ( ( $dots == true && $text != '' && strlen($text_temp) > $length ) ? '...' : ''); 
-	}
-?>
 
-
-
-<?php if(uw_list_pages()){ ?>
+<?php if(function_exists('uw_list_pages')){ ?>
 	<div id="mobile-sidebar">
-		<button id="mobile-sidebar-menu" class="visible-xs" aria-hidden="true" tabindex="1"> 
-	    	<div aria-hidden="true" id="ham"> 
-		    	<span></span> 
-				<span></span> 
-				<span></span> 
-				<span></span>        
+		<button id="mobile-sidebar-menu" class="visible-xs" aria-hidden="true" tabindex="1">
+	    	<div aria-hidden="true" id="ham">
+		    	<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
 			</div>
 	   		<div id="mobile-sidebar-title" class="page_item">
 				<?php echo text_cut(); ?>
@@ -34,4 +17,3 @@
 <?php } ?>
 
 <?php the_content(); ?>
-
--- inc/template-functions.php
+++ inc/template-functions.php
@@ -364,3 +364,21 @@ if ( !function_exists('uw_site_title')):
     }
 
 endif;
+
+if ( !function_exists('text_cut') ):
+
+    function text_cut($text = '', $length = 27, $dots = true) {
+      global $post;
+      $parent = get_post($post->post_parent);
+      $text = $parent->post_title;
+      $text = trim(preg_replace('#[\s\n\r\t]{2,}#', ' ', $text));
+      $text_temp = $text;
+      while (substr($text, $length, 1) != " ") {
+        $length--;
+        if ($length > strlen($text)) { break; }
+      }
+      $text = substr($text, 0, $length);
+      return $text . ( ( $dots == true && $text != '' && strlen($text_temp) > $length ) ? '...' : '');
+    }
+
+endif;
