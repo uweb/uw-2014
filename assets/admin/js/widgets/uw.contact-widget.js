e9206d0946158d54ee36b8bfeb6fc13985fd7249
Lauren V Caple <lcaple@uw.edu>
2018-11-14 18:03:08 -0800

    fixing contact widget bug on customize interface

--- assets/admin/js/widgets/uw.contact-widget.js
+++ assets/admin/js/widgets/uw.contact-widget.js
@@ -2,24 +2,23 @@ jQuery(document).ready(function( $ ) {
 
 
 
-  $('body').on('click', '#sidebar #person-button', function(event) {
-
+  $('body').on('click', '#person-button', function(event) {
+    event.preventDefault();
     var $id = $(this).closest('.widget').attr('id');
     var $number = $id.split("_contact-list-").pop();
 
-    $(this).parent().find('.uw-contact-card-contact-group').append(
-      _.template( $('#sidebar #redirect-template').html() ) ( { number : $number } )
+    $(this).parent().find('#the-people').append(
+      _.template( $('#redirect-template').html() ) ( { number : $number } )
     );
 
 
 
   })
 
-  // removes a contact and automatically saves the change 
-  $('body').on('click', '#sidebar .remove', function(event) {
+  // removes a contact and automatically saves the change
+  $('body').on('click', '.uw-contact-card-remove', function(event) {
         this.closest('.peep').remove();
         $("#widget-contact-list-2-savewidget").trigger('click');
- 	})
-
+  })
 
 })
