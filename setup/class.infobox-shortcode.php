10c4452f69000e02ae42647f9f4b77b0709f2402
umac10 <umac10@uw.edu>
2019-05-17 10:44:52 -0700

    add infobox shortcode button to tinyMCE

---
+++ setup/class.infobox-shortcode.php
@@ -0,0 +1,64 @@
+<?php
+
+Class UW_InfoboxShortcode {
+
+    function __construct()
+    {
+        add_shortcode( 'infobox', array($this, 'infobox_shortcode' ));
+        add_filter('mce_external_plugins', array($this, 'infobox_register_tinymce_plugin')); 
+        add_filter('mce_buttons', array($this, 'infobox_add_tinymce_button'));
+    }
+
+    // Add infobox shortcode.
+    function infobox_shortcode($atts, $content = null) 
+    {
+        $infobox_atts = shortcode_atts( array(
+            'color' => '',
+            'alignment' => '',
+        ), $atts );
+
+        $output = '';
+  
+        $output .= '<div class="info-box';
+
+        if ($infobox_atts['alignment'] != '') {
+            if ($infobox_atts['alignment'] == 'left') {
+                $output .= ' info-left';
+            } else if ($infobox_atts['alignment'] == 'right') {
+                $output .= ' info-right';
+            } else if ($infobox_atts['alignment'] == 'full-width') {
+                $output .= ' info-full';
+            }
+        }
+
+        $output .= '" ';
+
+        if ($infobox_atts['color'] != '') {
+            $output .= 'style="border-top-color:' . esc_html__($infobox_atts['color']) . '"';
+        }
+
+        $output .= '>';
+
+        if (!is_null($content)) {
+            $output .= apply_filters('the_content', $content);
+        }
+
+        $output .= '</div>';
+    
+        return $output;
+    }
+
+    // Register the plugin
+    function infobox_register_tinymce_plugin($plugin_array) 
+    {
+       $plugin_array['infobox_button'] = get_stylesheet_directory_uri() .'/js/uw.infobox.js';
+       return $plugin_array;
+    }
+    
+    // Add button to the toolbar.
+    function infobox_add_tinymce_button($buttons) 
+    {
+       $buttons[] = "infobox_button";
+       return $buttons;
+    }
+}
