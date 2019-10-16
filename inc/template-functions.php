d22be083cefe73688f38832f25cc70fad42ecb9d
Lauren V Caple <lcaple@uw.edu>
2019-10-16 16:07:43 -0700

    meta tag adjustments for cms

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -401,7 +401,23 @@ if ( !function_exists('uw_meta_tags') ):
     global $post;
     // Get the current site's URL
     $url = network_site_url();
-    if($url = "http://localhost/cms/" || $url = "http://cmsdev.u.washington.edu/" || $url = "https://www.washington.edu/cms/"){
+    $site_url = home_url();
+    if($url = "http://localhost/cms/" || $url = "http://cms.local/" || $url = "http://cmsdev.u.washington.edu/" || $url = "https://www.washington.edu/cms/"){
+      if ($site_url == "http://cms.local/uwclimatesurvey") {
+        $og_img = "https://s3-us-west-2.amazonaws.com/uw-s3-cdn/wp-content/uploads/sites/164/2019/10/16193323/Campus-Climate-Survey-Social-Facebook-1200x630.jpg";
+        $twitter_img = "https://s3-us-west-2.amazonaws.com/uw-s3-cdn/wp-content/uploads/sites/164/2019/09/27181721/logo.png";
+
+        echo '<meta property="og:image" content="' . $og_img . '" />' . PHP_EOL;
+        echo '<meta name="twitter:image" content="' . $twitter_img . '" />' . PHP_EOL;
+      }
+      else if(!has_post_thumbnail( $post->ID )) { //the post does not have featured image, use a default image
+          $default_image = "http://s3-us-west-2.amazonaws.com/uw-s3-cdn/wp-content/uploads/sites/10/2019/06/21094817/Univ-of-Washington_Memorial-Way.jpg"; //replace this with a default image on your server or an image in your media library
+          echo '<meta property="og:image" content="' . $default_image . '" />' . PHP_EOL;
+      }
+      else{
+        $thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full-content' );
+        echo '<meta property="og:image" content="' . esc_attr( $thumbnail_src[0] ) . '" />' . PHP_EOL;
+      }
 
       echo '<meta name="twitter:card" content="summary" />' . PHP_EOL;
       echo '<meta name="twitter:site" content="@uw" />' . PHP_EOL;
@@ -429,14 +445,6 @@ if ( !function_exists('uw_meta_tags') ):
 
       echo '<meta property="og:description" content="' . $fb_desc . '" />' . PHP_EOL;
       if (isset($post->type_meta) && $post->type_meta == 'article' && isset($post->author_meta) && $post->author_meta != '') { '<meta property="article:author" content="' . $post->author_meta . '" />' . PHP_EOL; }
-      if(!has_post_thumbnail( $post->ID )) { //the post does not have featured image, use a default image
-          $default_image = "http://s3-us-west-2.amazonaws.com/uw-s3-cdn/wp-content/uploads/sites/10/2019/06/21094817/Univ-of-Washington_Memorial-Way.jpg"; //replace this with a default image on your server or an image in your media library
-          echo '<meta property="og:image" content="' . $default_image . '" />' . PHP_EOL;
-      }
-      else{
-        $thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full-content' );
-        echo '<meta property="og:image" content="' . esc_attr( $thumbnail_src[0] ) . '" />' . PHP_EOL;
-      }
       echo "
       " . PHP_EOL;
     }
