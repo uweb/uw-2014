3b48cc9ff3a7dd42fad661d2c25c80a9e0f4c775
umac10 <umac10@uw.edu>
2020-02-28 16:00:52 -0800

    add option to upload youtube api key

--- setup/class.youtube-shortcode.php
+++ setup/class.youtube-shortcode.php
@@ -13,6 +13,8 @@ class UW_YouTube
 
     function __construct()
     {
+        add_action('admin_init', array($this, 'youtube_api_key_setting_init'));
+        add_action('wp_enqueue_scripts', array($this, 'youtube_api_key_localize_script'));
         add_shortcode('youtube', array($this, 'youtube_handler'));
     }
 
@@ -49,5 +51,51 @@ class UW_YouTube
         }
 
         return $return;
+    }
+
+    function youtube_api_key_setting_init() {
+        register_setting(
+            'media',
+            'youtube_api_key',
+            array($this, 'sanitize_youtube_api_key')
+        );
+
+        add_settings_section(
+            'youtube_api_key_section',
+            'Youtube API Key',
+            array($this, 'youtube_api_key_setting_description'),
+            'media'
+        );
+
+        add_settings_field(
+            'youtube_api_key_setting',
+            'API Key',
+            array($this, 'youtube_api_key_input'),
+            'media',
+            'youtube_api_key_section'
+        );
+    }
+
+    function youtube_api_key_setting_description() {
+        echo "<p>Upload your own YouTube API key below</p>";
+    }
+
+    function youtube_api_key_input() {
+        $option = get_option('youtube_api_key');
+        ?>
+            <input name="youtube_api_key" type="text" class="regular-text" value="<?php echo esc_attr($option); ?>" />
+        <?php
+    }
+    
+    function sanitize_youtube_api_key($key) {
+        return preg_replace('/[^a-zA-Z0-9_\-]/', '', $key);
+    }
+
+    function youtube_api_key_localize_script() {
+        $api_key = array(
+            'youtube' => get_option('youtube_api_key')
+        );
+        
+        wp_localize_script('site', 'apiKey', $api_key);
     }
 }
